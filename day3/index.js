0fba809e95ec96bc293394c509e099977a150a01
Jim Pouwels <jim.pouwels@philips.com>
2022-12-06 11:32:15 +0100

    day3

---
+++ day3/index.js
@@ -0,0 +1,34 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+const input = fs.readFileSync(path.join(process.cwd(), 'input.txt')).toString();
+const rucksacks = input.split('\n');
+
+const lowerCaseAsciiOffset = 96; // maps to ascii '1'
+const upperCaseAsciiOffset = 38; // maps to ascii '65'
+
+const duplicates = [];
+
+for (const rucksack of rucksacks) {
+    const half = Math.ceil(rucksack.length / 2);    
+    const comp1 = rucksack.slice(0, half);
+    const comp2 = rucksack.slice(half);
+
+    comp1Loop: for (const itemA of comp1) {
+        for (const itemB of comp2) {
+            if (itemA === itemB) {
+                duplicates.push(itemA);
+                break comp1Loop;
+            }
+        }
+    }
+}
+
+const total = duplicates.reduce((total, i) => total + valueOf(i), 0);
+
+console.log(`Part 1: ${total}`);
+
+function valueOf(char) {
+    const value = char.charCodeAt(0);
+    return value - (value > 96 ? lowerCaseAsciiOffset : upperCaseAsciiOffset);
+}
