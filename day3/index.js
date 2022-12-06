c076b859fde94fad48af69194fbe6f6afc1d2201
Jim Pouwels <jim.pouwels@philips.com>
2022-12-06 12:04:45 +0100

    refactor

--- day3/index.js
+++ day3/index.js
@@ -7,14 +7,14 @@ const rucksacks = input.split('\n');
 const lowerCaseAsciiOffset = 96; // maps to ascii '1'
 const upperCaseAsciiOffset = 38; // maps to ascii '65'
 
-let duplicates = rucksacks.flatMap(rucksack => {
-    const half = Math.ceil(rucksack.length / 2);    
-    const comp1 = [...new Set(rucksack.slice(0, half))];
-    const comp2 = [...new Set(rucksack.slice(half))];
-    return comp1.filter(itemA => comp2.find(itemB => itemA === itemB));
-});
-
-const total = duplicates.reduce((total, i) => total + valueOf(i), 0);
+let total = rucksacks
+    .flatMap(rucksack => {
+        const half = Math.ceil(rucksack.length / 2);    
+        const comp1 = [...new Set(rucksack.slice(0, half))];
+        const comp2 = [...new Set(rucksack.slice(half))];
+        return comp1.filter(itemA => comp2.find(itemB => itemA === itemB));
+    })
+    .reduce((total, i) => total + valueOf(i), 0);
 
 console.log(`Part 1: ${total}`);
 
