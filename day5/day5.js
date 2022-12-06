7429254f925636bebe193eb22778b828c6088997
Jim Pouwels <jim.pouwels@philips.com>
2022-12-06 16:14:47 +0100

    parsing the crates

---
+++ day5/day5.js
@@ -0,0 +1,29 @@
+import assert from '../assert.js';
+import readLines from '../readlines.js';
+
+const lines = readLines('day5/input.txt');
+
+const stacks = parseStacks(lines);
+
+console.log(stacks);
+
+function parseStacks(lines) {
+    let stacks = []
+    for (const line of lines) {
+        if (!line.trim().startsWith('[')) {
+            break;
+        }
+        for (let i = 0; i < line.length; i += 4) {
+            if (!stacks[i/4]) {
+                stacks[i/4] = [];
+            }
+            let crate = line.slice(i, i + 4);
+            if (!crate.trim()) {
+                continue;
+            } else {
+                stacks[i/4].push(crate);
+            }
+        }
+    }
+    return stacks;
+}
