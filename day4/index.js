7bd550289e2fdcdba3d86a388f40961fbf909dc2
Jim Pouwels <jim.pouwels@philips.com>
2022-12-06 14:37:51 +0100

    refactor

--- day4/index.js
+++ day4/index.js
@@ -4,21 +4,35 @@ import * as path from 'path';
 const input = fs.readFileSync(path.join(process.cwd(), 'input.txt')).toString();
 const lines = input.split('\n');
 
-let total = lines.reduce((total, line) => {
+let numberCompletelyInclude = 0;
+let numberPartiallyInclude = 0;
+lines.forEach(line => {
+    const ranges = parseRanges(line);
+    if (ranges.range1.filter(r1 => ranges.range2.includes(r1)).length === ranges.range1.length ||
+        ranges.range2.filter(r2 => ranges.range1.includes(r2)).length === ranges.range2.length) {
+        numberCompletelyInclude++;
+    }
+    if (ranges.range1.find(r1 => ranges.range2.includes(r1)) ||
+        ranges.range2.find(r2 => ranges.range1.includes(r2))) {
+        numberPartiallyInclude++;
+    }
+}, 0);
+
+console.log(`Part 1: ${numberCompletelyInclude}`);
+console.log(`Part 2: ${numberPartiallyInclude}`);
+
+function parseRanges(line) {
     const ranges = line.split(',');
     const range1 = parseRange(ranges[0]);
     const range2 = parseRange(ranges[1]);
-
-    if ((range1.from >= range2.from && range1.to <= range2.to) ||
-        (range2.from >= range1.from && range2.to <= range1.to)) {
-        return total + 1;
-    }
-    return total;
-}, 0);
-
-console.log(`Part 1: ${total}`);
+    return { range1: range1, range2: range2 };
+}
 
 function parseRange(stringValue) {
     const parts = stringValue.split('-');
-    return { from: parseInt(parts[0]), to: parseInt(parts[1]) };
+    let range = [];
+    for (let i = parseInt(parts[0]); i < parseInt(parts[1]) + 1; i++) {
+        range.push(i);
+    }
+    return range;
 }
