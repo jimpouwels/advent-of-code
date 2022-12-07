6ce05986f8296f1070c3dfe9a3a2c66d6c6f6925
Jim Pouwels <jim.pouwels@philips.com>
2022-12-07 13:35:25 +0100

    day6 part 2

--- day6/day6.js
+++ day6/day6.js
@@ -1,10 +1,10 @@
-export default function day6(input) {
+export default function day6(input, distinctCharCount) {
     let startOfMarker = -1;
 
     for (let i = 0; i < input.length; i++) {
-        let mostRecent = input.slice(i, i + 4);
-        if ([...new Set(mostRecent)].length == 4) {
-            startOfMarker = i + 4;
+        let mostRecent = input.slice(i, i + distinctCharCount);
+        if ([...new Set(mostRecent)].length == distinctCharCount) {
+            startOfMarker = i + distinctCharCount;
             break;
         }
     }
