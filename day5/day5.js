6f3239260010d9b566677d03785df0d31a7c3132
Jim Pouwels <jim.pouwels@philips.com>
2022-12-07 14:20:26 +0100

    refactor day 6

--- day5/day5.js
+++ day5/day5.js
@@ -1,11 +1,11 @@
 export default function day5(input) {
     const moves = parseMoves(input);
-    const part1Stacks = rearrange(parseStacks(input), moves, true);
-    const part2Stacks = rearrange(parseStacks(input), moves, false);
-    
+    const part1Result = rearrange(parseStacks(input), moves, true);
+    const part2Result = rearrange(parseStacks(input), moves, false);
+
     return {
-        part1: topCratesAsString(part1Stacks),
-        part2: topCratesAsString(part2Stacks)
+        part1: topCratesAsString(part1Result),
+        part2: topCratesAsString(part2Result)
     };
 }
 
