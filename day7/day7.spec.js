0aef0a92f4bd1c5944737d89539a5f09937d8590
Jim Pouwels <jim.pouwels@philips.com>
2022-12-07 16:16:32 +0100

    day 7 part 1

---
+++ day7/day7.spec.js
@@ -0,0 +1,13 @@
+import { readLines } from '../readlines.js';
+import day7 from './day7.js';
+
+describe('day7', () => {
+
+    it('part1', () => {
+        expect(day7(readLines('day7/testdata.txt')).part1).toEqual(95437);
+    });
+
+    it('part2', () => {
+    });
+
+});
