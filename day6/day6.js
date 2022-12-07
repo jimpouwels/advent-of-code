1ed96b128be3973413ae960251a1f3fb40f8a33d
Jim Pouwels <jim.pouwels@philips.com>
2022-12-07 14:11:44 +0100

    refactor day 6

--- day6/day6.js
+++ day6/day6.js
@@ -1,8 +1,8 @@
 export default function day6(input, distinctCharCount) {
     let cursor;
     for (cursor = distinctCharCount - 1; cursor < input.length; cursor++) {
-        let mostRecent = input.slice(cursor - (distinctCharCount - 1), cursor + 1);
-        if ([...new Set(mostRecent)].length == distinctCharCount) {
+        let charsTillNow = input.slice(cursor - distinctCharCount + 1, cursor + 1);
+        if ([...new Set(charsTillNow)].length == distinctCharCount) {
             break;
         }
     }
