72d360557d0209a32bd2108f0bff1d90be526e15
Jim Pouwels <jim.pouwels@philips.com>
2022-12-07 12:58:27 +0100

    added day 6

---
+++ day6/day6.js
@@ -0,0 +1,7 @@
+export default function day6(input) {
+    
+    return {
+        part1: null,
+        part2: null
+    };
+}
---
+++ day6/day6.spec.js
@@ -0,0 +1,14 @@
+import { readLines } from '../readlines.js';
+import day6 from './day6.js';
+
+describe('day6', () => {
+
+    it('part1', () => {
+        const result = day6(readLines('day6/testdata.txt'));
+        expect(result.part1).toEqual('');
+    });
+
+    it('part2', () => {
+    });
+
+});
