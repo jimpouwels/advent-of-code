c88286b0ada7d544ca050c3743ea513d9567e10d
Jim Pouwels <jim.pouwels@philips.com>
2022-12-08 14:23:30 +0100

    delete

--- assert.js
+++
@@ -1,9 +0,0 @@
-
-export default function assert(expected, actual) {
-    const result = actual == expected;
-    if (!result) {
-        console.log(`FAILED: Expected ${expected}, was ${actual}`);
-    } else {
-        console.log(`SUCCESS`);
-    }
-}
