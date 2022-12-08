375a3caa16ddc773b7d0a5c9c5d694ec93e1686c
Jim Pouwels <jim.pouwels@philips.com>
2022-12-08 10:14:14 +0100

    refactor

--- day8/day8.js
+++ day8/day8.js
@@ -1,13 +1,13 @@
 export default function day8(input) {
     const forest = parseForest(input);
     
-    let numberOfVisibleTrees = forest.flatMap((treeRow, rowIndex) => treeRow.filter((tree, treeIndex) =>
+    const numberOfVisibleTrees = forest.flatMap((treeRow, rowIndex) => treeRow.filter((tree, treeIndex) =>
             isOuterTree(treeRow, rowIndex, treeIndex, forest) ||
             isVisibleFromLeft(treeRow, treeIndex, tree) ||
             isVisibleFromRight(treeRow, treeIndex, tree) ||
             isVisibleFromTop(forest, rowIndex, treeIndex, tree) ||
             isVisibleFromBottom(forest, rowIndex, treeIndex, tree
-    ))).length; 
+    ))).length;
 
     return {
         part1: numberOfVisibleTrees,
@@ -24,11 +24,24 @@ function isVisibleFromTop(forest, rowIndex, treeIndex, tree) {
 }
 
 function isVisibleFromRight(treeRow, treeIndex, tree) {
-    return Math.max(...treeRow.slice(treeIndex + 1, treeRow.length)) < tree;
+    return Math.max(...getTreesToRight(treeRow, treeIndex)) < tree;
 }
 
 function isVisibleFromLeft(treeRow, treeIndex, tree) {
-    return Math.max(...treeRow.slice(0, treeIndex)) < tree;
+    return Math.max(...getTreesToLeft(treeRow, treeIndex)) < tree;
+}
+
+function isOuterTree(treeRow, rowIndex, treeIndex, forest) {
+    return rowIndex == 0 || rowIndex == forest.length - 1 || 
+           treeIndex == 0 || treeIndex == treeRow.length - 1;
+}
+
+function getTreesToLeft(treeRow, treeIndex) {
+    return treeRow.slice(0, treeIndex).reverse();
+}
+
+function getTreesToRight(treeRow, treeIndex) {
+    return treeRow.slice(treeIndex + 1, treeRow.length);
 }
 
 function getTreesAbove(forest, rowIndex, treeIndex) {
@@ -39,11 +52,6 @@ function getTreesAbove(forest, rowIndex, treeIndex) {
 function getTreesBelow(forest, rowIndex, treeIndex) {
     return forest.filter(row => forest.indexOf(row) > rowIndex)
                  .map(row => row[treeIndex]);
-}
-
-function isOuterTree(treeRow, rowIndex, treeIndex, forest) {
-    return rowIndex == 0 || rowIndex == forest.length - 1 || 
-           treeIndex == 0 || treeIndex == treeRow.length - 1;
 }
 
 function parseForest(input) {
