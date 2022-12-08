25811eca21f57b240a35798a7cff5edfbffb03fe
Jim Pouwels <jim.pouwels@philips.com>
2022-12-08 15:33:21 +0100

    refactor

--- 2021/day2/day2.js
+++ 2021/day2/day2.js
@@ -1,21 +1,25 @@
 export default function run(input) {
     const location = { horizontal: 0, depth: 0 };
 
-    input.map(line => {
-        const split = line.split(' ');
-        return { direction: split[0], distance: parseInt(split[1]) };
-    }).forEach(move => {
-        switch (move.direction) {
-            case 'forward':
-                location.horizontal += move.distance;
-                break;
-            case 'up':
-                location.depth -= move.distance;
-                break;
-            case 'down':
-                location.depth += move.distance;
-        }
-    });
+    input.map(line => parseMove(line)).forEach(move => performMove(move, location));
 
     return location.horizontal * location.depth;
+}
+
+function performMove(move, location) {
+    switch (move.direction) {
+        case 'forward':
+            location.horizontal += move.distance;
+            break;
+        case 'up':
+            location.depth -= move.distance;
+            break;
+        case 'down':
+            location.depth += move.distance;
+    }
+}
+
+function parseMove(line) {
+    const split = line.split(' ');
+    return { direction: split[0], distance: parseInt(split[1]) };
 }
