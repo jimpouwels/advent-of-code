3dd3f82da6da4fe57d8a2f45f91b31371641bd10
Jim Pouwels <jim.pouwels@philips.com>
2022-12-08 09:38:45 +0100

    refactor

--- day8/day8.spec.js
+++ day8/day8.spec.js
@@ -4,8 +4,8 @@ import day8 from './day8.js';
 describe('day8', () => {
 
     it('part1', () => {
-        expect(day8(readLines('day8/testdata.txt')).part1).toEqual(21);
-        expect(day8(readLines('day8/testdata1.txt')).part1).toEqual(1763);
+        expect(day8(readLines('day8/testdata_p1_1.txt')).part1).toEqual(21);
+        expect(day8(readLines('day8/testdata_p1_2.txt')).part1).toEqual(1763);
     });
 
     it('part2', () => {
