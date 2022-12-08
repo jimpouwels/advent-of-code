f96a6d0d2e50ab2c55f0b0580eed758279fd13b8
Jim Pouwels <jim.pouwels@philips.com>
2022-12-08 14:48:54 +0100

    refactor

--- 2021/day1/day1.js
+++ 2021/day1/day1.js
@@ -1,7 +1,7 @@
 export default function day1(input) {
 
     return {
-        part1: input.filter((x, i) => i < input.length - 1 ? x < input[i + 1] : false ).length,
+        part1: input.filter((x, i) => i < input.length - 1 ? x < input[i + 1] : false).length,
         part2: 0
     }
 }
