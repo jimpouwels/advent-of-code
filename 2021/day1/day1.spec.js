cebf6108c622743c206b4369ef6d620f860d0170
Jim Pouwels <jim.pouwels@philips.com>
2022-12-08 14:35:58 +0100

    2021 day1 part1

---
+++ 2021/day1/day1.spec.js
@@ -0,0 +1,13 @@
+import { readLines } from '../../common/readlines.js';
+import day1 from './day1.js';
+
+describe('day1', () => {
+
+    it('part1', () => {
+        expect(day1(readLines('2021/day1/testdata.txt')).part1).toEqual(7);
+    });
+
+    it('part2', () => {
+    });
+
+});
--- 2022/day1/day1.spec.js
+++ 2022/day1/day1.spec.js
@@ -4,13 +4,11 @@ import day1 from './day1.js';
 describe('day1', () => {
 
     it('part1', () => {
-        const result = day1(readFile('2022/day1/testdata.txt'));
-        expect(result.part1).toEqual(30);
+        expect(day1(readFile('2022/day1/testdata.txt')).part1).toEqual(30);
     });
 
     it('part2', () => {
-        const result = day1(readFile('2022/day1/testdata.txt'));
-        expect(result.part2).toEqual(81);
+        expect(day1(readFile('2022/day1/testdata.txt')).part2).toEqual(81);
     });
 
 });
