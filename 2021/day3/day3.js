c4164e1b99301b7cbd0cbfe2085183fbc1ecb267
Jim Pouwels <jim.pouwels@philips.com>
2022-12-08 19:47:43 +0100

    part 2

--- 2021/day3/day3.js
+++ 2021/day3/day3.js
@@ -1,20 +1,30 @@
 export default function run(input) {
     const gamma = getGamma(input);
     const epsilon = inverseOf(gamma);
+    const oxygen = getLifeSupportRating(input, false);
+    const co2 = getLifeSupportRating(input, true);
+
     return {
         part1: parseInt(gamma, 2) * parseInt(epsilon, 2),
-        part2: 0
+        part2: parseInt(oxygen, 2) * parseInt(co2, 2)
     }
 }
 
 function getGamma(input) {
-    return [...input[0]].flatMap((_bit, index) => 
-                            countInColumns(input, index, "1") > input.length / 2 ? "1" : "0")
+    return [...input[0]].flatMap((_bit, bitIndex) => 
+                            countInColumns(input, bitIndex, "1") > input.length / 2 ? "1" : "0")
                         .join('');
 }
 
-function countInColumns(input, index, bit) {
-    return input.reduce((sum, line) => sum + (line[index] === bit ? 1 : 0), 0);
+function getLifeSupportRating(input, inverse, column = 0) {
+    let mostCommon = countInColumns(input, column, "1") >= input.length / 2 ? "1" : "0";
+    mostCommon = inverse ? inverseOf(mostCommon) : mostCommon;
+    const remainingNumbers = input.filter(number => number[column] === mostCommon);
+    return remainingNumbers.length > 1 ? getLifeSupportRating(remainingNumbers, inverse, column + 1) : remainingNumbers.join('');
+}
+
+function countInColumns(input, bitIndex, bit) {
+    return input.reduce((sum, line) => sum + (line[bitIndex] === bit ? 1 : 0), 0);
 }
 
 function inverseOf(value) {
