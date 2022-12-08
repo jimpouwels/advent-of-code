47e3a79b9d7122c32c853e11adfb564d63b5b302
Jim Pouwels <jim.pouwels@philips.com>
2022-12-08 15:07:02 +0100

    refactor

--- 2021/day1/day1.js
+++ 2021/day1/day1.js
@@ -1,6 +1,6 @@
 export default function day1(input, windowSize) {
 
-    const increasedCount = input.filter((_x, i) => i <= input.length - windowSize)
+    const increasedCount = input.filter((_x, i) => i < input.length - windowSize)
                                 .filter((_x, i) => {
                                     const current = sumWindow(input, i, i + windowSize);
                                     const next = sumWindow(input, i + 1, i + windowSize + 1);
