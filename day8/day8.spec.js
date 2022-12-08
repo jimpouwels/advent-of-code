25e41c78cb9d76f2400bb86d987043b9349cdf58
Jim Pouwels <jim.pouwels@philips.com>
2022-12-08 10:48:31 +0100

    day8 part 2

--- day8/day8.spec.js
+++ day8/day8.spec.js
@@ -4,7 +4,7 @@ import day8 from './day8.js';
 describe('day8', () => {
 
     it('part1', () => {
-        // expect(day8(readLines('day8/testdata.txt')).part1).toEqual(21);
+        expect(day8(readLines('day8/testdata.txt')).part1).toEqual(21);
     });
 
     it('part2', () => {
