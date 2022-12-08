512abc3a51562c68d6e2b17059a47c6eb400fb3e
Jim Pouwels <jim.pouwels@philips.com>
2022-12-08 17:28:44 +0100

    refactor

--- 2021/day3/day3.spec.js
+++ 2021/day3/day3.spec.js
@@ -4,7 +4,7 @@ import run from './day3.js';
 describe('day3', () => {
 
     it('part1', () => {
-        expect(run(readLines('2021/day3/testdata.txt')).part1).toEqual(0);
+        expect(run(readLines('2021/day3/testdata.txt')).part1).toEqual(198);
     });
 
     it('part2', () => {
