7715a51fc669411cfa404f151a1c4ba426caf119
Jim Pouwels <jim.pouwels@philips.com>
2022-12-08 15:43:43 +0100

    2021 day2 part2

--- 2021/day2/day2.js
+++ 2021/day2/day2.js
@@ -1,25 +1,42 @@
 export default function run(input) {
-    const location = { horizontal: 0, depth: 0 };
+    const locationPart1 = { horizontal: 0, depth: 0 };
+    input.map(line => parseMove(line)).forEach(move => performMove(move, locationPart1));
 
-    input.map(line => parseMove(line)).forEach(move => performMove(move, location));
-
-    return location.horizontal * location.depth;
+    const locationPart2 = { horizontal: 0, depth: 0, aim: 0 };
+    input.map(line => parseMove(line)).forEach(move => performMoveWithAim(move, locationPart2));
+    return {
+        part1: locationPart1.horizontal * locationPart1.depth,
+        part2: locationPart2.horizontal * locationPart2.depth
+    }
 }
 
 function performMove(move, location) {
     switch (move.direction) {
         case 'forward':
-            location.horizontal += move.distance;
+            location.horizontal += move.value;
             break;
         case 'up':
-            location.depth -= move.distance;
+            location.depth -= move.value;
             break;
         case 'down':
-            location.depth += move.distance;
+            location.depth += move.value;
+    }
+}
+function performMoveWithAim(move, location) {
+    switch (move.direction) {
+        case 'forward':
+            location.horizontal += move.value;
+            location.depth += (location.aim * move.value);
+            break;
+        case 'up':
+            location.aim -= move.value;
+            break;
+        case 'down':
+            location.aim += move.value;
     }
 }
 
 function parseMove(line) {
     const split = line.split(' ');
-    return { direction: split[0], distance: parseInt(split[1]) };
+    return { direction: split[0], value: parseInt(split[1]) };
 }
--- 2021/day2/day2.spec.js
+++ 2021/day2/day2.spec.js
@@ -4,10 +4,11 @@ import run from './day2.js';
 describe('day2', () => {
 
     it('part1', () => {
-        expect(run(readLines('2021/day2/testdata.txt'))).toEqual(150);
+        expect(run(readLines('2021/day2/testdata.txt')).part1).toEqual(150);
     });
 
     it('part2', () => {
+        expect(run(readLines('2021/day2/testdata.txt')).part2).toEqual(900);
     });
 
 });
