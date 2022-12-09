d9c685ac3a7d2f57b955b39cfc3ab6ab98c7b0a2
Jim Pouwels <jim.pouwels@philips.com>
2022-12-09 11:39:13 +0100

    refactor

--- 2022/day9/day9.js
+++ 2022/day9/day9.js
@@ -6,60 +6,60 @@ export default function run(lines, numberOfKnots) {
 
     deltas.forEach(delta => {
         move(head, delta);
-        let pointToFollow = head;
+        let knotToFollow = head;
         knots.forEach(tail => {
-            follow(tail, pointToFollow);
-            pointToFollow = tail;
+            follow(tail, knotToFollow);
+            knotToFollow = tail;
         });
     });
     return tail.visitedPlaces.length;
 }
 
-function follow(point, pointToFollow) {
-    followX(point, pointToFollow);
-    followY(point, pointToFollow);
-    if (!point.visitedPlaces.find(p => p.x == point.x && p.y == point.y)) {
-        point.visitedPlaces.push({ x: point.x, y: point.y });
+function follow(knot, knotToFollow) {
+    followX(knot, knotToFollow);
+    followY(knot, knotToFollow);
+    if (!knot.visitedPlaces.find(p => p.x == knot.x && p.y == knot.y)) {
+        knot.visitedPlaces.push({ x: knot.x, y: knot.y });
     }
 }
 
-function followX(point, pointToFollow) {
-    if (distanceX(point, pointToFollow) > 1) {
-        if (distanceY(point, pointToFollow) > 0) {
-            moveTowardsY(point, pointToFollow);
+function followX(knot, knotToFollow) {
+    if (distanceX(knot, knotToFollow) > 1) {
+        if (distanceY(knot, knotToFollow) > 0) {
+            moveTowardsY(knot, knotToFollow);
         }
-        moveTowardsX(point, pointToFollow);
+        moveTowardsX(knot, knotToFollow);
     }
 }
 
-function followY(point, pointToFollow) {
-    if (distanceY(point, pointToFollow) > 1) {
-        if (distanceX(point, pointToFollow) > 0) {
-            moveTowardsX(point, pointToFollow);
+function followY(knot, knotToFollow) {
+    if (distanceY(knot, knotToFollow) > 1) {
+        if (distanceX(knot, knotToFollow) > 0) {
+            moveTowardsX(knot, knotToFollow);
         }
-        moveTowardsY(point, pointToFollow);
+        moveTowardsY(knot, knotToFollow);
     }
 }
 
-function moveTowardsX(point, pointToFollow) {
-    move(point, { x: pointToFollow.x > point.x ? 1 : -1, y: 0 });
+function moveTowardsX(knot, knotToFollow) {
+    move(knot, { x: knotToFollow.x > knot.x ? 1 : -1, y: 0 });
 }
 
-function moveTowardsY(point, pointToFollow) {
-    move(point, { x: 0, y: pointToFollow.y > point.y ? 1 : -1 });
+function moveTowardsY(knot, knotToFollow) {
+    move(knot, { x: 0, y: knotToFollow.y > knot.y ? 1 : -1 });
 }
 
-function distanceY(point1, point2) {
-    return Math.abs(point1.y - point2.y);
+function distanceY(knot1, knot2) {
+    return Math.abs(knot1.y - knot2.y);
 }
 
-function distanceX(point1, point2) {
-    return Math.abs(point1.x - point2.x);
+function distanceX(knot1, knot2) {
+    return Math.abs(knot1.x - knot2.x);
 }
 
-function move(point1, point2) {
-    point1.x += point2.x;
-    point1.y += point2.y;
+function move(knot1, knot2) {
+    knot1.x += knot2.x;
+    knot1.y += knot2.y;
 }
 
 function parseMoves(lines) {
@@ -68,19 +68,19 @@ function parseMoves(lines) {
         const value = parseInt(split[1]);
         switch (split[0]) {
             case 'R':
-                return repeatPoint({ x: 1, y: 0 }, value);
+                return createDelta({ x: 1, y: 0 }, value);
             case 'L':
-                return repeatPoint({ x: -1, y: 0 }, value);
+                return createDelta({ x: -1, y: 0 }, value);
             case 'U':
-                return repeatPoint({ x: 0, y: 1 }, value);
+                return createDelta({ x: 0, y: 1 }, value);
             case 'D':
-                return repeatPoint({ x: 0, y: -1 }, value);
+                return createDelta({ x: 0, y: -1 }, value);
         }
     });
 }
 
-function repeatPoint(point, times) {
-    return new Array(times).fill(0).map(_t => { return point });
+function createDelta(delta, times) {
+    return new Array(times).fill(0).map(_t => { return delta });
 }
 
 function createKnots(count) {
