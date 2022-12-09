26abb8517b4939eb74fe0a48c1645c38588b2937
Jim Pouwels <jim.pouwels@philips.com>
2022-12-09 09:59:18 +0100

    day8 part 1

--- 2022/day9/day9.js
+++ 2022/day9/day9.js
@@ -1,17 +1,30 @@
 export default function run(lines) {
     const moves = parseMoves(lines);
     const head = { x: 0, y: 0 };
-    const tail = { x: 0, y: 0 };
+    const tail = { x: 0, y: 0, visitedPlaces: [] };
+    
     moves.forEach(move => {
         add(head, move);
         follow(tail, head);
     });
-    console.log(head);
-    return 0;
+    return tail.visitedPlaces.length;
 }
 
 function follow(point, pointToFollow) {
-
+    if (Math.abs(pointToFollow.x - point.x) > 1) {
+        if (Math.abs(pointToFollow.y - point.y) > 0) {
+            point.y += pointToFollow.y > point.y ? 1 : -1;    
+        }
+        point.x += pointToFollow.x > point.x ? 1 : -1;
+    } else if (Math.abs(pointToFollow.y - point.y) > 1) {
+        if (Math.abs(pointToFollow.x - point.x) > 0) {
+            point.x += pointToFollow.x > point.x ? 1 : -1;    
+        }
+        point.y += pointToFollow.y > point.y ? 1 : -1;
+    }
+    if (!point.visitedPlaces.find(p => p.x == point.x && p.y == point.y)) {
+        point.visitedPlaces.push({ x: point.x, y: point.y} );
+    }
 }
 
 function add(point1, point2) {
@@ -20,18 +33,22 @@ function add(point1, point2) {
 }
 
 function parseMoves(lines) {
-    return lines.map(line => {
+    return lines.flatMap(line => {
         const split = line.split(' ');
         const value = parseInt(split[1]);
         switch (split[0]) {
-            case 'U':
-                return { x: 0, y: value};
-            case 'D':
-                return { x: 0, y: -value };
-            case 'L':
-                return { x: -value, y: 0 };
             case 'R':
-                return { x: value, y: 0 };
+                return repeatPoint({ x: 1, y: 0 }, value);
+            case 'L':
+                return repeatPoint({ x: -1, y: 0 }, value);
+            case 'U':
+                return repeatPoint({ x: 0, y: 1 }, value);
+            case 'D':
+                return repeatPoint({ x: 0, y: -1 }, value);
         }
     });
+}
+
+function repeatPoint(point, times) {
+    return new Array(times).fill(0).map(_t => { return point });
 }
