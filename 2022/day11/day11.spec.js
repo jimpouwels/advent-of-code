f3b59f99096c9066d541a9857319adb58727df0f
Jim Pouwels <jim.pouwels@philips.com>
2022-12-11 10:22:01 +0100

    day11

---
+++ 2022/day11/day11.spec.js
@@ -0,0 +1,13 @@
+import { readLines } from '../../common/readlines.js';
+import run from './day11.js';
+
+describe('day11', () => {
+
+    it('part1', () => {
+        expect(run(readLines('2022/day11/testdata.txt')).part1).toEqual(0);
+    });
+
+    it('part2', () => {
+    });
+
+});
