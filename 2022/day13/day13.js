2b765e10ea3ccbef6ff6a8b94b8921d2584ac228
Jim Pouwels <jim.pouwels@philips.com>
2022-12-13 20:21:23 +0100

    simple case

--- 2022/day13/day13.js
+++ 2022/day13/day13.js
@@ -1,11 +1,22 @@
 export default function run(input) {
     const pairs = parseLines(input);
 
-    console.log(pairs);
+    console.log(compare(pairs[0]));
+
     return {
         part1: 0,
         part2: 0
     };
+}
+
+function compare(pair) {
+    let isCorrect = true;
+    for (let i = 0; i < pair.left.length(); i++) {
+        if (pair.right.values[i] < pair.left.values[i]) {
+            isCorrect = false;
+        }
+    }
+    return isCorrect;
 }
 
 function parseLines(lines) {
@@ -18,5 +29,50 @@ function parseLines(lines) {
 }
 
 function parseLine(line) {
-    return line;
+    let cursor = 0;
+    let root, currentList = null;
+    while (cursor < line.length) {
+        const char = readChar(line, cursor++);
+        switch (char) {
+            case '[':
+                const subList = new List(currentList);
+                if (!root) {
+                    root = subList;
+                    currentList = root;
+                } else {
+                    currentList.push(subList);
+                    currentList = subList;
+                }
+                break;
+            case ']':
+                currentList = currentList.parent;
+                break;
+            case ',':
+                break;
+            default:
+                currentList.push(parseInt(char));
+        }
+    }
+    return root;
+}
+
+function readChar(line, cursor) {
+    return line.charAt(cursor);
+}
+
+class List {
+    values = [];
+    parent;
+
+    constructor(parent) {
+        this.parent = parent;
+    }
+    
+    push(value) {
+        this.values.push(value);
+    }
+
+    length() {
+        return this.values.length;
+    }
 }
