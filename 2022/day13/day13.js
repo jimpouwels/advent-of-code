d38ac54302d1f9b330a58dfb228512074266d358
Jim Pouwels <jim.pouwels@philips.com>
2022-12-13 15:59:04 +0100

    parse pairs, but not yet the values

--- 2022/day13/day13.js
+++ 2022/day13/day13.js
@@ -1,6 +1,22 @@
-export default function run(lines) {
+export default function run(input) {
+    const pairs = parseLines(input);
+
+    console.log(pairs);
     return {
         part1: 0,
         part2: 0
     };
+}
+
+function parseLines(lines) {
+    return lines.split('\n\n').map(pair => { 
+        const splittedPair = pair.split('\n');
+        const left = parseLine(splittedPair[0]);
+        const right = parseLine(splittedPair[1]);
+        return { left: left, right: right };
+    });
+}
+
+function parseLine(line) {
+    return line;
 }
--- 2022/day13/day13.spec.js
+++ 2022/day13/day13.spec.js
@@ -1,10 +1,10 @@
-import { readLines } from '../../common/readlines.js';
+import { readFile } from '../../common/readlines.js';
 import run from './day13.js';
 
 describe('day13', () => {
 
     it('run', () => {
-        const result = run(readLines('2022/day13/testdata.txt'));
+        const result = run(readFile('2022/day13/testdata.txt'));
         expect(result.part1).toEqual(0);
     });
 
