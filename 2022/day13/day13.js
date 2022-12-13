721c7e68aa56b925b0ec79047b9a7e587909c67d
Jim Pouwels <jim.pouwels@philips.com>
2022-12-13 15:40:35 +0100

    empty day13

---
+++ 2022/day13/day13.js
@@ -0,0 +1,6 @@
+export default function run(lines) {
+    return {
+        part1: 0,
+        part2: 0
+    };
+}
---
+++ 2022/day13/day13.spec.js
@@ -0,0 +1,11 @@
+import { readLines } from '../../common/readlines.js';
+import run from './day13.js';
+
+describe('day13', () => {
+
+    it('run', () => {
+        const result = run(readLines('2022/day13/testdata.txt'));
+        expect(result.part1).toEqual(0);
+    });
+
+});
