a8ccf15e1ed2a6084a5f96065e2b65911323e1dd
Jim Pouwels <jim.pouwels@philips.com>
2022-12-14 14:13:46 +0100

    refactor

--- 2022/day13/day13.js
+++ 2022/day13/day13.js
@@ -2,16 +2,8 @@ const distressMarkers = [[[2]], [[6]]];
 
 export default function run(input) {
     const pairs = parseLines(input);
-
-    const part1 = pairs.reduce((sum, pair, index) => {
-        if (compareLists(pair) === 1) {
-            return sum += index + 1;
-        }
-        return sum;
-    }, 0);
-
-    const sorted = [...pairs.flat(), ...distressMarkers]
-                           .sort((line1, line2) => -compareLists([line1, line2]));                         
+    const part1 = pairs.reduce((sum, pair, index) => compare(pair) === 1 ? sum += index + 1 : sum, 0);
+    const sorted = [...pairs.flat(), ...distressMarkers].sort((line1, line2) => -compare([line1, line2]));                         
 
     return {
         part1: part1,
@@ -19,7 +11,7 @@ export default function run(input) {
     };
 }
 
-function compareLists([left, right]) {
+function compare([left, right]) {
     const result = left.length < right.length ? 1 : 0;
     for (let i = 0; i < left.length; i++) {
         if (i == right.length) {
@@ -27,20 +19,24 @@ function compareLists([left, right]) {
         }
         let leftItem = left[i];
         let rightItem = right[i];
-        if (typeof leftItem === "number" && typeof rightItem === "number") {
+        if (isNumber(leftItem) && isNumber(rightItem)) {
             const result = leftItem === rightItem ? 0 : leftItem < rightItem ? 1 : -1;
             if (result != 0) {
                 return result;
             }
         } else {
-            let result = compareLists([typeof leftItem === "number" ? [leftItem] : leftItem, 
-                                    typeof rightItem === "number" ? [rightItem] : rightItem]);
+            let result = compare([isNumber(leftItem) ? [leftItem] : leftItem, 
+                                       isNumber(rightItem) ? [rightItem] : rightItem]);
             if (result != 0) {
                 return result;
             }
         }
     }
     return result;
+}
+
+function isNumber(item) {
+    return typeof item === "number";
 }
 
 function parseLines(lines) {
