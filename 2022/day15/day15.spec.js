62f0d7fda3d36421fc581fbbde65d07c84d5fcc5
Jim Pouwels <jim.pouwels@philips.com>
2022-12-16 08:26:43 +0100

    refactor

--- 2022/day15/day15.spec.js
+++ 2022/day15/day15.spec.js
@@ -3,10 +3,16 @@ import run from './day15.js';
 
 describe('day15', () => {
 
-    it('run1', () => {
-        const r = run(readLines('2022/day15/testdata.txt'), 2000000);
-        expect(r.part1).toEqual(5127797);
-        expect(r.part2).toEqual(12518502636475);
+    it('run1-small', () => {
+        const r = run(readLines('2022/day15/testdata-small.txt'), 10);
+        expect(r.part1).toEqual(26);
+        expect(r.part2).toEqual(56000011);
     });
+
+    // it('run1-big', () => {
+    //     const r = run(readLines('2022/day15/testdata-big.txt'), 2000000);
+    //     expect(r.part1).toEqual(5127797);
+    //     expect(r.part2).toEqual(12518502636475);
+    // });
 
 });
