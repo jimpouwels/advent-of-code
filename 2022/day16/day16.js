2e67eebeb1f9fee1d0c0a7cae9b509e7fe728ca6
Jim Pouwels <jim.pouwels@philips.com>
2022-12-17 17:44:29 +0100

    parsing valves

--- 2022/day16/day16.js
+++ 2022/day16/day16.js
@@ -1,7 +1,18 @@
 export default function run(lines) {
-    console.log(lines);
-
+    const valves = parseValves(lines);
+    console.log(valves);
     return {
         part1: 0
     }
+}
+
+function parseValves(lines) {
+    return lines.map(line => {
+        const { valve, rate, targets } = line.match(/Valve (?<valve>([A-Z]+)) has flow rate=(?<rate>(\d+)); tunnel[s]? lead[s]? to valve[s]? (?<targets>[A-Z, ]+)/).groups;
+        return {
+            name: valve,
+            rate: rate,
+            targets: targets.split(', ')
+        }
+    });
 }
