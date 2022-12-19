069759b1e89a3764bc983a61f00d7bd3044b4c23
Jim Pouwels <jim.pouwels@philips.com>
2022-12-19 16:03:24 +0100

    refactor

--- 2021/day5/day5.js
+++ 2021/day5/day5.js
@@ -2,18 +2,23 @@ import { toPointsList } from "../../common/geo.js";
 
 export default function run(lines) {
     const points = parsePoints(lines);
+    const grid = createAndFillGrid(points);
+    
+    return {
+        part1: grid.reduce((sum, val) => sum + val.reduce((sum, val) => sum + (val >= 2 ? 1 : 0), 0), 0),
+        part2: 0
+    }
+}
+
+function createAndFillGrid(points) {
     const maxX = Math.max(...points.map(p => p.x));
     const maxY = Math.max(...points.map(p => p.y));
-
     const grid = new Array(maxX + 1);
     for (let x = 0; x < grid.length; x++) {
         grid[x] = new Array(maxY + 1).fill(0);
     }
     points.forEach(point => grid[point.x][point.y] += 1);
-    return {
-        part1: grid.reduce((sum, val) => sum + val.reduce((sum, val) => sum + (val >= 2 ? 1 : 0), 0), 0),
-        part2: 0
-    }
+    return grid;
 }
 
 function parsePoints(lines) {
