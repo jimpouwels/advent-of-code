de577458deed6a9616d99a1f8d0efad8a0da4c7c
Jim Pouwels <jim.pouwels@philips.com>
2022-12-20 10:36:33 +0100

    refactor

--- 2021/day5/day5.js
+++ 2021/day5/day5.js
@@ -1,30 +1,40 @@
 import { toPointsList } from "../../common/geo.js";
 
 export default function run(lines) {
-    const points = parsePoints(lines);
-    const grid = createAndFillGrid(points);
+    let points = parsePoints(lines);
+    let grid = createGrid(points);
+
+    points.forEach(point => grid[point.x][point.y] += 1);
     
+    const part1 = grid.reduce((sum, val) => sum + val.reduce((sum, val) => sum + (val >= 2 ? 1 : 0), 0), 0);
+
+    points = parsePoints(lines, true);
+    grid = createGrid(points);
+
+    points.forEach(point => grid[point.x][point.y] += 1);
+
+    const part2 = grid.reduce((sum, val) => sum + val.reduce((sum, val) => sum + (val >= 2 ? 1 : 0), 0), 0);
+
     return {
-        part1: grid.reduce((sum, val) => sum + val.reduce((sum, val) => sum + (val >= 2 ? 1 : 0), 0), 0),
-        part2: 0
+        part1: part1,
+        part2: part2,
     }
 }
 
-function createAndFillGrid(points) {
+function createGrid(points) {
     const maxX = Math.max(...points.map(p => p.x));
     const maxY = Math.max(...points.map(p => p.y));
-    const grid = new Array(maxX + 1);
+    const grid = new Array(maxX + 1).fill();
     for (let x = 0; x < grid.length; x++) {
         grid[x] = new Array(maxY + 1).fill(0);
     }
-    points.forEach(point => grid[point.x][point.y] += 1);
     return grid;
 }
 
-function parsePoints(lines) {
+function parsePoints(lines, includeDiagonal = false) {
     return lines.flatMap(line => {
         const { x1, y1, x2, y2 } = line.match(/(?<x1>(\d+)),(?<y1>(\d+)) -> (?<x2>(\d+)),(?<y2>(\d+))/).groups;
-        if (x1 != x2 && y1 != y2) {
+        if (!includeDiagonal && x1 != x2 && y1 != y2) {
             return [];
         }
         return toPointsList({ x: +x1, y: +y1 }, { x: +x2, y: +y2 });
--- common/geo.js
+++ common/geo.js
@@ -1,11 +1,21 @@
 export function toPointsList(point1, point2) {
     const allPoints = [];
-    const linePoints = [ point1, point2 ].sort((a, b) => (a.x - b.x) + (a.y - b.y));
-
-    for (let x = linePoints[0].x; x <= linePoints[1].x; x++) {
-        for (let y = linePoints[0].y; y <= linePoints[1].y; y++) {
-            allPoints.push({ x: x, y: y });
-        }
+    
+    let diffX = 0, diffY = 0;
+    if (point1.x < point2.x) {
+        diffX = 1;
+    } else if (point1.x > point2.x) {
+        diffX = -1;
     }
+    if (point1.y < point2.y) {
+        diffY = 1;
+    } else if (point1.y > point2.y) {
+        diffY = -1;
+    }
+
+    for (let x = point1.x, y = point1.y; x != point2.x || y != point2.y; x += diffX, y += diffY) {
+        allPoints.push({ x: x, y: y });
+    }
+    allPoints.push(point2);
     return allPoints;
 }
