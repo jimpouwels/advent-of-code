b11f28cfbb7e2c1fe927e719726e2766d0f9cebd
Jim Pouwels <jim.pouwels@philips.com>
2022-12-20 14:20:09 +0100

    refactor

--- 2021/day6/day6.js
+++ 2021/day6/day6.js
@@ -1,10 +1,10 @@
 export default function run(lines, days) {
-    const fishMap = new Array(9).fill(0);
-    JSON.parse(`[${lines}]`).forEach(f => fishMap[f]++);
+    const fishCounts = new Array(9).fill(0);
+    JSON.parse(`[${lines}]`).forEach(f => fishCounts[f]++);
 
     for (let i = 0; i < days; i++) {
-        const temp = fishMap[0];
-        fishMap.forEach((_f, i) => fishMap[i] = i == 8 ? temp: i == 6 ? fishMap[i + 1] + temp : fishMap[i + 1]);
+        const temp = fishCounts[0];
+        fishCounts.forEach((_f, i) => fishCounts[i] = i == 8 ? temp: i == 6 ? fishCounts[i + 1] + temp : fishCounts[i + 1]);
     }
-    return fishMap.reduce((sum, val) => sum + val);
+    return fishCounts.reduce((sum, val) => sum + val);
 }
