53cb9b6d43afc95f1c5c3122a4f1d3f84bcb6b2a
Jim Pouwels <jim.pouwels@philips.com>
2022-12-20 10:48:19 +0100

    refactor

--- common/geo.js
+++ common/geo.js
@@ -1,17 +1,8 @@
 export function toPointsList(point1, point2) {
     const allPoints = [];
-    
-    let diffX = 0, diffY = 0;
-    if (point1.x < point2.x) {
-        diffX = 1;
-    } else if (point1.x > point2.x) {
-        diffX = -1;
-    }
-    if (point1.y < point2.y) {
-        diffY = 1;
-    } else if (point1.y > point2.y) {
-        diffY = -1;
-    }
+
+    const diffX = point1.x < point2.x ? 1 : point1.x > point2.x ? -1 : 0;
+    const diffY = point1.y < point2.y ? 1 : point1.y > point2.y ? -1 : 0;
 
     for (let x = point1.x, y = point1.y; x != point2.x || y != point2.y; x += diffX, y += diffY) {
         allPoints.push({ x: x, y: y });
