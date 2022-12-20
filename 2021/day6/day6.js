80175b922bb66a2f6b23726e18dde3c435ff8aca
Jim Pouwels <jim.pouwels@philips.com>
2022-12-20 13:45:20 +0100

    refactor

--- 2021/day6/day6.js
+++ 2021/day6/day6.js
@@ -1,19 +1,25 @@
-export default function run(lines) {
+export default function run(lines, days) {
     const fish = JSON.parse(`[${lines}]`);
 
-    for (let i = 0; i < 80; i++) {
-        fish.forEach((f, index, fish) => {
-            if (f == 0) {
-                fish[index] = 6;
-                fish.push(8);
-            } else {
-                fish[index]--;
-            }
-        });
+    const fishMap = new Array(9).fill(0);
+    fish.forEach(f => {
+        if (!fishMap[f]) {
+            fishMap[f] = 0;
+        }
+        fishMap[f]++;
+    })
+    for (let i = 0; i < days; i++) {
+        const temp = fishMap[0];
+        fishMap[0] = fishMap[1];
+        fishMap[1] = fishMap[2];
+        fishMap[2] = fishMap[3];
+        fishMap[3] = fishMap[4];
+        fishMap[4] = fishMap[5];
+        fishMap[5] = fishMap[6];
+        fishMap[6] = fishMap[7] + temp;
+        fishMap[7] = fishMap[8];
+        fishMap[8] = temp;
     }
 
-    return {
-        part1: fish.length,
-        part2: 0,
-    }
+    return fishMap.reduce((sum, val) => sum + val);
 }
--- 2021/day6/day6.spec.js
+++ 2021/day6/day6.spec.js
@@ -4,8 +4,7 @@ import run from './day6.js';
 describe('day6', () => {
 
     it('part1', () => {
-        expect(run(readFile('2021/day6/testdata.txt')).part1).toEqual(5934);
-        // expect(run(readLines('2021/day6/testdata.txt')).part2).toEqual(12);
+        expect(run(readFile('2021/day6/testdata.txt'), 80)).toEqual(5934);
     });
 
 });
