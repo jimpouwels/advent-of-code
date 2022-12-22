d76208170c179274cd45a2430761924614ed1cfe
Jim Pouwels <jim.pouwels@philips.com>
2022-12-22 13:06:24 +0100

    refactor

--- 2022/day16/day16.spec.js
+++ 2022/day16/day16.spec.js
@@ -8,7 +8,7 @@ describe('day16', () => {
         expect(r.part1).toEqual(1651);
     });
 
-    // Takes around 15 seconds to run, uncomment if you want
+    // Takes around 7 seconds to run, uncomment if you want
     // it('run1-big', () => {
     //     const r = run(readLines('2022/day16/testdata-big.txt'));
     //     expect(r.part1).toEqual(2114);
