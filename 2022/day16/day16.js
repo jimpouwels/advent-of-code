b5689a5bfa50d1c8c8dc42defeec76b8f6e71f7f
Jim Pouwels <jim.pouwels@philips.com>
2022-12-22 20:15:05 +0100

    refactor

--- 2022/day16/day16.js
+++ 2022/day16/day16.js
@@ -7,7 +7,7 @@ export default function run(lines) {
     const routes = [];
     findShortestRoutes(valves, routes);
     return {
-        part1: calculateHighestPressure(valves.find(x => x.name === 'AA'), valves.filter(v => v.rate > 0), routes, 30)
+        part1: calculateHighestPressure(valves.find(v => v.name === 'AA'), valves.filter(v => v.rate > 0), routes, 30)
     }
 }
 
