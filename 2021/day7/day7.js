a8c3f3ff93dd4e4ecb78fca7ad08f3d4f3b37f75
Jim Pouwels <jim.pouwels@philips.com>
2022-12-23 12:31:23 +0100

    refactor

--- 2021/day7/day7.js
+++ 2021/day7/day7.js
@@ -1,13 +1,18 @@
 export default function run(input) {
     const crabs = JSON.parse(`[${input}]`);
 
+    crabs.sort((a, b) => a - b);
+
+    const min = crabs[0];
+    const max = crabs.slice(-1);
+
     let part1 = Infinity;
-    for (let i = Math.min(...crabs); i <= Math.max(...crabs); i++) {
+    for (let i = min; i <= max; i++) {
         part1 = Math.min(crabs.reduce((sum, val) => sum + Math.abs(val - i), 0), part1);
     }
     let part2 = Infinity;
-    for (let i = Math.min(...crabs); i <= Math.max(...crabs); i++) {
-        part2 = Math.min(crabs.reduce((sum, val) => sum + new Array(Math.abs(val - i)).fill(0).reduce((sum, val, index) => sum + (index + 1), 0), 0), part2);
+    for (let i = min; i <= max; i++) {
+        part2 = Math.min(crabs.reduce((sum, val) => sum + new Array(Math.abs(val - i)).fill(0).reduce((sum, _val, index) => sum + (index + 1), 0), 0), part2);
     }
     return {
         part1: part1,
