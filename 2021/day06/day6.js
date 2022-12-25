02f9296b50787b6dc99f77e9f4f8e993ae0dc5d4
Jim Pouwels <jim.pouwels@philips.com>
2022-12-25 09:02:55 +0100

    fix

--- 2021/day06/day6.js
+++ 2021/day06/day6.js
@@ -3,7 +3,7 @@ export default function run(lines, days) {
     JSON.parse(`[${lines}]`).forEach(f => fishCounts[f]++);
 
     for (let i = 0; i < days; i++) {
-        fishCounts[fishCounts.push(fishCounts.shift())] += fishCounts[8];
+        fishCounts[fishCounts.push(fishCounts.shift()) - 3] += fishCounts[8];
     }
     return fishCounts.reduce((sum, val) => sum + val);
 }
