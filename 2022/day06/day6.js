a7e14cb9015d9b10493076634b5a2003ac135752
Jim Pouwels <jim.pouwels@philips.com>
2022-12-26 22:25:37 +0100

    refactor

--- 2022/day06/day6.js
+++ 2022/day06/day6.js
@@ -1,7 +1,7 @@
 export default function run(input, distinctCharCount) {
     let cursor;
     for (cursor = 0; cursor < input.length; cursor++) {
-        let charsTillNow = input.slice(cursor, cursor + distinctCharCount);
+        const charsTillNow = input.slice(cursor, cursor + distinctCharCount);
         if ([...new Set(charsTillNow)].length == distinctCharCount) {
             return cursor + distinctCharCount;
         }
