377ab1f48f5af34e1beaa9b91f652bc9d31553dd
Jim Pouwels <jim.pouwels@philips.com>
2022-12-29 23:21:46 +0100

    refactor

--- 2021/day08/day8.js
+++ 2021/day08/day8.js
@@ -1,15 +1,15 @@
 export default function run(lines) {
-    const digits = lines.map(line => {
+    const displays = lines.map(line => {
         const splitted = line.split('|');
-        return new Line(splitted[0].trim().split(' '), splitted[1].trim().split(' '));
+        return new Display(splitted[0].trim().split(' '), splitted[1].trim().split(' '));
     });
 
     const lengths = [ 2, 3, 4, 7 ];
 
-    while (digits.find(d => d.mapping.length < 10)) {
-        digits.forEach(digit => {
-            const convertedDigits = digit.mapping;
-            digit.input.forEach(input => {
+    while (displays.find(d => d.mapping.length < 10)) {
+        displays.forEach(display => {
+            const convertedDigits = display.mapping;
+            display.input.forEach(input => {
                 if (convertedDigits.find(c => c.matches(input))) {
                     return;
                 }
@@ -21,31 +21,31 @@ export default function run(lines) {
                     convertedDigits.push(new Digit(input, 7));
                 } else if (input.length === 7) {
                     convertedDigits.push(new Digit(input, 8));
-                } else if (input.length === 5 && digit.contains(7, input)) {
+                } else if (input.length === 5 && display.isContainedBy(7, input)) {
                     convertedDigits.push(new Digit(input, 3));
-                } else if (input.length === 6 && digit.doesNotContain(3, input) && digit.contains(7, input)) {
+                } else if (input.length === 6 && display.isNotContainedBy(3, input) && display.isContainedBy(7, input)) {
                     convertedDigits.push(new Digit(input, 0));
-                } else if (input.length === 6 && digit.contains(3, input)) {
+                } else if (input.length === 6 && display.isContainedBy(3, input)) {
                     convertedDigits.push(new Digit(input, 9));
-                } else if (input.length === 5 && convertedDigits.find(c => c.decimal === 6 && c.contains(input))) {
+                } else if (input.length === 5 && display.contains(6, input)) {
                     convertedDigits.push(new Digit(input, 5));
-                } else if (input.length === 5 && convertedDigits.find(c => c.decimal === 5 && !c.matches(input))) {
+                } else if (input.length === 5 && display.doesNotMatch(5, input)) {
                     convertedDigits.push(new Digit(input, 2));
-                } else if (input.length === 6 && digit.doesNotContain(7, input)) {
+                } else if (input.length === 6 && display.isNotContainedBy(7, input)) {
                     convertedDigits.push(new Digit(input, 6));
                 }
             });
-            digit.mapping = convertedDigits;
+            display.mapping = convertedDigits;
         });
     }
 
     return {
-        part1: digits.reduce((sum, val) => sum + val.output.reduce((sum, val) => sum + (lengths.includes(val.length) ? 1 : 0), 0), 0),
-        part2: digits.reduce((sum, digit) => sum + +digit.output.reduce((sum, val) => sum + digit.mapping.find(c => c.matches(val)).decimal, ''), 0),
+        part1: displays.reduce((sum, val) => sum + val.output.reduce((sum, val) => sum + (lengths.includes(val.length) ? 1 : 0), 0), 0),
+        part2: displays.reduce((sum, digit) => sum + +digit.output.reduce((sum, val) => sum + digit.mapping.find(c => c.matches(val)).decimal, ''), 0),
     }
 }
 
-class Line {
+class Display {
     mapping = [];
     input = [];
     output = [];
@@ -55,12 +55,20 @@ class Line {
         this.output = output;
     }
 
-    contains(decimal, otherInput) {
+    isContainedBy(decimal, otherInput) {
         return this.mapping.find(c => decimal === c.decimal && c.isContainedBy(otherInput));
     }
 
-    doesNotContain(decimal, otherInput) {
+    isNotContainedBy(decimal, otherInput) {
         return this.mapping.find(c => decimal === c.decimal && !c.isContainedBy(otherInput));
+    }
+
+    contains(decimal, otherInput) {
+        return this.mapping.find(c => decimal === c.decimal && c.contains(otherInput));
+    }
+
+    doesNotMatch(decimal, otherInput) {
+        return this.mapping.find(c => decimal === c.decimal && !c.matches(otherInput));
     }
 }
 
