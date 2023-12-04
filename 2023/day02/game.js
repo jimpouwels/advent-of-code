ff06c6c529357de4d78143fe54aa5cbb77e9ef66
Jim Pouwels <jim.pouwels@gmail.com>
2023-12-04 16:52:18 +0100

    2023; day2; part2

--- 2023/day02/game.js
+++ 2023/day02/game.js
@@ -8,16 +8,4 @@ export default class Game {
         this.gameNumber = gameNumber;
         this.draws = draws;
     }
-
-    lowestDraw() {
-        let red = Number.MIN_VALUE;
-        let blue = Number.MIN_VALUE;
-        let green = Number.MIN_VALUE;
-        this.draws.forEach(draw => {
-            red = draw.red() > red ? draw.red() : red;
-            blue = draw.blue() > blue ? draw.blue() : blue;
-            green = draw.green() > green ? draw.green() : green;
-        });
-        return new Draw(red, blue, green);
-    }
 }
