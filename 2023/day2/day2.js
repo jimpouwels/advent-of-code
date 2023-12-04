6ef2f62035d21790edef43c99d74a55cdf11ce86
Jim Pouwels <jim.pouwels@gmail.com>
2023-12-04 10:05:58 +0100

    2023: day2;part1

--- 2023/day2/day2.js
+++ 2023/day2/day2.js
@@ -1,19 +1,14 @@
 export default function run(lines, cubes) {
-    
-    let games = parseGames(lines);
-
-    let total = games.filter(game =>
-        game.draws.filter(d => {
-                return d.red() > cubes["red"] || d.green() > cubes["green"] || d.blue() > cubes["blue"];
-            }).length == 0
-        ).reduce((sum, possibleGame) => sum + possibleGame.gameNumber, 0);
+    let maxDraw = new Draw(cubes["red"], cubes["blue"], cubes["green"]);
+    let total = parseGames(lines).filter(game => game.draws.filter(d => !d.fits(maxDraw)).length == 0)
+                                 .reduce((sum, possibleGame) => sum + possibleGame.gameNumber, 0);
 
     function parseGames(lines) {
         return lines.map(line => {
             let match = line.match(/Game (\d+): (.*)/);
             let draws = match[2].split('; ');
             let parsedDraws = draws.map(draw => {
-                let parsedDraw = new Draw();
+                let parsedDraw = new Draw(0, 0, 0);
                 draw.split(', ').map(d => {
                     let split = d.split(' ');
                     parsedDraw.add(split[1], parseInt(split[0]));
@@ -44,6 +39,21 @@ class Draw {
         "red": 0,
         "blue": 0,
         "green": 0
+    };
+
+    constructor(red, blue, green) {
+        this.cubes["red"] = red;
+        this.cubes["blue"] = blue;
+        this.cubes["green"] = green;
+    }
+
+    fits(otherDraw) {
+        // console.log(this);
+        // console.log("vs");
+        // console.log(otherDraw);
+        return this.red() <= otherDraw.red() &&
+               this.green() <= otherDraw.green() &&
+               this.blue() <= otherDraw.blue();
     }
 
     red() {
