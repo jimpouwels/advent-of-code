c0496b23b5af0395a6acce45f43b5726e8623103
Jim Pouwels <jim.pouwels@gmail.com>
2023-12-05 11:15:43 +0100

    2023; day4; part2

--- 2023/day01/day1.js
+++ 2023/day01/day1.js
@@ -1,15 +1,6 @@
 export default function run(input) {
-    const writtenNumbers = {
-        "one": "1",
-        "two": "2",
-        "three": "3",
-        "four": "4",
-        "five": "5",
-        "six": "6",
-        "seven": "7",
-        "eight": "8",
-        "nine": "9"
-    }
+    
+    const writtenNumbers = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9" }
 
     return {
         part1: calibration(input, getFirstNumberIn),
