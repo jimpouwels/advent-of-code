b7ff80702c770046455dba8e9ec44b61b9822018
Jim Pouwels <jim.pouwels@gmail.com>
2023-12-06 13:23:26 +0100

    refactor

--- 2023/day05/map.js
+++ 2023/day05/map.js
@@ -21,13 +21,13 @@ export default class Map {
             if (!inlet) {
                 outRanges.push(new Range(range.from, range.to));
             } else {
-                // check range that falls to right of inlet
-                if (range.from < inlet.from && range.to <= inlet.to) {
+                // check range that falls to left of inlet
+                if (range.from < inlet.from) {
                     outRanges = outRanges.concat(this.findOutletsByRanges([new Range(range.from, inlet.from - 1)]));
                     range.from = inlet.from;
                 }
-                // check range that falls to left of inlet
-                if (range.from >= inlet.from && range.to > inlet.to) {
+                // check range that falls to right of inlet
+                if (range.to > inlet.to) {
                     outRanges = outRanges.concat(this.findOutletsByRanges([new Range(inlet.to + 1, range.to)]));
                     range.to = inlet.to + 1;
                 }
