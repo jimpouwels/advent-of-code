a50b057235406e8a938e6ec27893e310a9917fe4
Jim Pouwels <jim.pouwels@gmail.com>
2023-12-08 08:49:25 +0100

    day8-p1

--- 2023/day08/day8.js
+++ 2023/day08/day8.js
@@ -3,12 +3,12 @@ export default function run(lines) {
     let instructions = parseInstructions(lines[0]);
     let elements = parseElements(lines.slice(2));
 
-    let current = elements.filter(e => e.name == 'AAA')[0];
+    let current = elements.filter(e => e.isBegin())[0];
     let stepCount = 0;
     let instructionIndex = 0;
-    while (current.name != 'ZZZ') {
+    while (!current.isEnd()) {
         let leftOrRight = instructions[instructionIndex++];
-        current = elements.filter(e => e. name == current.next[leftOrRight])[0];
+        current = current.next[leftOrRight];
         if (instructionIndex == instructions.length) {
             instructionIndex = 0;
         }
@@ -26,19 +26,35 @@ function parseInstructions(line) {
 }
 
 function parseElements(lines) {
-    return lines.map(line => {
-        const { element, left, right } = line.match(/(?<element>.*) = \((?<left>.*), (?<right>.*)\)/).groups
-        return new Instruction(element, left, right);
+    let elements = lines.map(line => {
+        const { element } = line.match(/(?<element>.*) =/).groups
+        return new Instruction(element);
     });
+    lines.forEach((line, i) => {
+        const { left, right } = line.match(/= \((?<left>.*), (?<right>.*)\)/).groups;
+        elements[i].setNext(elements.filter(e => e.name == left)[0], elements.filter(e => e.name == right)[0]);
+    });
+    return elements;
 }
 
 class Instruction {
     name;
     next = [];
 
-    constructor(name, left, right) {
+    constructor(name) {
         this.name = name;
+    }
+    
+    setNext(left, right) {
         this.next.push(left);
         this.next.push(right);
+    }
+
+    isBegin() {
+        return this.name == 'AAA';
+    }
+
+    isEnd() {
+        return this.name == 'ZZZ';
     }
 }
