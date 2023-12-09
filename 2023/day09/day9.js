aa3baa9fdee96bee791d88919cf751b57f2e38c2
Jim Pouwels <jim.pouwels@gmail.com>
2023-12-09 12:06:39 +0100

    day9

--- 2023/day09/day9.js
+++ 2023/day09/day9.js
@@ -2,31 +2,20 @@ export default function run(lines) {
     let histories = parseHistories(lines);
 
     return {
-        part1: histories.map(history => calculateFuture([...history])).reduce((sum, val) => sum + val, 0),
-        part2: histories.map(history => calculatePast(history)).reduce((sum, val) => sum + val, 0)
+        part1: histories.map(history => extrapolate([...history])).reduce((sum, val) => sum + val, 0),
+        part2: histories.map(history => extrapolate([...history], true)).reduce((sum, val) => sum + val, 0)
     }
 }
 
-function calculateFuture(history) {
-    let differences = [];
-    for (let i = 0; i < history.length - 1; i++) {
-        differences.push(history[i + 1] - history[i]);
+function extrapolate(history, backwards = false) {
+    let differences = history.slice(0, -1).map((h, i) => {
+        return history[i + 1] - h;
+    });
+    let result = backwards ? history.shift(): history.pop();
+    if (!differences.every(d => d == 0)) {
+        result += extrapolate(differences, backwards) * (backwards ? -1 : 1);
     }
-    if (differences.every(d => d == 0)) {
-        return history.pop();
-    }
-    return history[history.length - 1] + calculateFuture(differences);
-}
-
-function calculatePast(history) {
-    let differences = [];
-    for (let i = 0; i < history.length - 1; i++) {
-        differences.push(history[i + 1] - history[i]);
-    }
-    if (differences.every(d => d == 0)) {
-        return history[0];
-    }
-    return history[0] - calculatePast(differences);
+    return result;
 }
 
 function parseHistories(lines) {
