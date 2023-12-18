f8bcc0b71a7930e618a11f5ed5fbd70782e25ccf
Jim Pouwels <jim.pouwels@philips.com>
2023-12-18 13:30:23 +0100

    day12-p1

--- 2023/day12/day12.js
+++ 2023/day12/day12.js
@@ -4,14 +4,13 @@ let logger = Logger.getLogger('2023-day12');
 
 export default function run(lines) {
     let total = 0;
-    lines.forEach((line, i) => {
-        logger.log('puzzle ' + i);
+    lines.forEach(line => {
         const { arrangementString, groupsString } = line.match(/(?<arrangementString>.*) (?<groupsString>.*)/).groups;
         let groups = groupsString.split(',').map(g => parseInt(g));
         let arrangement = arrangementString.split('');
         
     
-        let combos = doIt(arrangement, groups, 1);
+        let combos = getCombinations(arrangement, groups, 1);
     
         let count = 0;
         combos.forEach(c => {
@@ -30,51 +29,43 @@ export default function run(lines) {
     return total;
 }
 
-function doIt(remainingArrangement, remainingGroups, level = 9999) {
+function getCombinations(remainingArrangement, remainingGroups) {
     let combos = [];
     if (remainingGroups.length > 0) {
-        for (let i = 0; i < remainingArrangement.length; i++) {
-            let req = i + (remainingGroups.length - 1) + remainingGroups.reduce((sum, val) => sum + val, 0);
+        for (let gapCount = 0; gapCount < remainingArrangement.length; gapCount++) {
+            let req = gapCount + (remainingGroups.length - 1) + remainingGroups.reduce((sum, val) => sum + val, 0);
             if (req > remainingArrangement.length) {
                 continue;
             }
-            let posCount = 0;
+            let writeIndex = 0;
             let combo1 = [];
-            for (let j = 0; j < i; j++) {
+            for (let j = 0; j < gapCount; j++) {
                 combo1.push('.');
-                posCount++
+                writeIndex++
             }
             for (let j = 0; j < remainingGroups[0]; j++) {
                 combo1.push('#');
-                posCount++
+                writeIndex++
             }
             if (remainingGroups.length > 1) {
-                posCount++;
+                writeIndex++;
                 combo1.push('.');
-            } 
+            }
+
+            // add tail
             if (remainingGroups.length == 1) {
-                for (let j = 0; j < remainingArrangement.length - posCount; j++) {
+                for (let j = 0; j < remainingArrangement.length - writeIndex; j++) {
                     combo1.push('.');
                 }
                 combos.push(combo1);
                 continue;
             }
-            let requiredPlaces = (remainingGroups.filter((r, i) => i > 0).length - 1) + remainingGroups.filter((r, i) => i > 0).reduce((sum, val) => sum + val, 0);
-            if (requiredPlaces <= 0) { 
-                combos.push[combo1];
-                continue;
-            }
-            let availablePlaces = remainingArrangement.length - posCount;
-            if (requiredPlaces <= availablePlaces) {
-                let more = doIt(remainingArrangement.slice(posCount), remainingGroups.slice(1));
-                more.forEach(m => {
-                    let copyArr = [...combo1];
-                    copyArr = copyArr.concat(m);
-                    combos.push(copyArr);
-                });
-            } else {
-                return [combo1];
-            }
+            let more = getCombinations(remainingArrangement.slice(writeIndex), remainingGroups.slice(1));
+            more.forEach(m => {
+                let copyArr = [...combo1];
+                copyArr = copyArr.concat(m);
+                combos.push(copyArr);
+            });
         }
     } 
     return combos;
--- 2023/day12/day12.spec.js
+++ 2023/day12/day12.spec.js
@@ -4,7 +4,7 @@ import run from './day12.js';
 describe('day11', () => {
 
     it('runs', () => {
-        expect(run(readLines('2023/day12/testdata.txt'))).toEqual(21);
+        expect(run(readLines('2023/day12/testdata.txt'))).toEqual(7653);
     });
 
 });
