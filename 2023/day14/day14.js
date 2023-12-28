fcbb2e51d6ffe594d4022164a2682e9873dc04a8
Jim Pouwels <jim.pouwels@philips.com>
2023-12-28 19:13:52 +0100

    day14-p2

--- 2023/day14/day14.js
+++ 2023/day14/day14.js
@@ -1,35 +1,32 @@
-import Logger from "../../common/logger";
-
-let logger = Logger.getLogger('2023-day14');
-
 const direction = {
-    North: {yAdd: -1, xAdd: 0},
-    West: {yAdd: 0, xAdd: -1},
-    South: {yAdd: 1, xAdd: 0},
-    East: {yAdd: 0, xAdd: 1}
+    North: {moveY: -1, moveX: 0},
+    West: {moveY: 0, moveX: -1},
+    South: {moveY: 1, moveX: 0},
+    East: {moveY: 0, moveX: 1}
 };
-
 let instructions = [direction.North, direction.West, direction.South, direction.East];
 
-let cache = [];
 
 export default function run(lines, cycles) {
     let platform = lines.map(l => l.split(''));
     
+    // Part 1
     let platformPart1 = platform.map(r => r.map(d => d));
     tilt(platformPart1, direction.North);
-
+    
+    // Part 2
     let platformPart2 = platform.map(r => r.map(d => d));
     let count = 0;
+    let seen = [];
     while (count < cycles) {
         tiltRound(platformPart2);
         
         let tiltResult = JSON.stringify(platformPart2);
-        let seenIndex = cache.indexOf(tiltResult);
+        let seenIndex = seen.indexOf(tiltResult);
         if (seenIndex >= 0) {
             count = cycles - (cycles - count) % (count - seenIndex);
         } else {
-            cache.push(tiltResult);
+            seen.push(tiltResult);
         }
         count++;
     }
@@ -62,15 +59,15 @@ function sum(platform) {
 function tilt(platform, instruction) {
     for (let y = 0; y < platform.length; y++) {
         for (let x = 0; x < platform[0].length; x++) {
-            let targetX = instruction.xAdd > 0 ? platform[0].length - 1 - x : x;
-            let targetY = instruction.yAdd > 0 ? platform.length - 1 - y : y;
+            let targetX = instruction.moveX > 0 ? platform[0].length - 1 - x : x;
+            let targetY = instruction.moveY > 0 ? platform.length - 1 - y : y;
             if (platform[targetY][targetX] !== 'O') continue;
             let nextX = targetX;
             let nextY = targetY;
             platform[targetY][targetX] = '.';
             while (true) {
-                nextX += instruction.xAdd;
-                nextY += instruction.yAdd;
+                nextX += instruction.moveX;
+                nextY += instruction.moveY;
                 if (nextX < 0 || nextY < 0 || nextX == platform[0].length || nextY == platform.length || ['O', '#'].includes(platform[nextY][nextX])) {
                     break;
                 }
