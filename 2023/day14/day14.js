54d5d82b0b1e4e6e8a84fba42de0bad460af89be
Jim Pouwels <jim.pouwels@philips.com>
2023-12-28 12:51:27 +0100

    day14-p2

--- 2023/day14/day14.js
+++ 2023/day14/day14.js
@@ -2,27 +2,59 @@ import Logger from "../../common/logger";
 
 let logger = Logger.getLogger('2023-day14');
 
-export default function run(lines) {
+const direction = {
+    North: {name: "north", yAdd: -1, xAdd: 0},
+    West: {name: "west", yAdd: 0, xAdd: -1},
+    South: {name: "south", yAdd: 1, xAdd: 0},
+    East: {name: "east", yAdd: 0, xAdd: 1}
+};
+
+let instructions = [direction.North, direction.West, direction.South, direction.East];
+
+export default function run(lines, cycles) {
     let platform = lines.map(l => l.split(''));
-    logger.logGrid(platform, (g) => g);
     
-    platform.forEach((row, y) => {
-        row.forEach((_, x) => {
-            if (y === 0 || platform[y][x] !== 'O') return;
-            let nextY = y;
-            platform[y][x] = '.';
-            while (true) {
-                if ((nextY - 1) < 0 || ['O', '#'].includes(platform[nextY - 1][x])) {
-                    break;
-                }
-                nextY--;
-            }
-            platform[nextY][x] = 'O';
-        });
-    });
+    let timesMill = 0;
+    let count = 0;
+    while (count < cycles) {
+        tilt(platform, instructions[0]);
+        tilt(platform, instructions[0]);
+        tilt(platform, instructions[0]);
+        tilt(platform, instructions[0]);
+        if (count % 1000000 == 0) {
+            timesMill++;
+            logger.log(timesMill);
+        }
+        count++;
+    }
+    logger.logGrid(platform, (g) => g);
     return platform.reverse().reduce((sum, row, i) => {
         return sum + row.reduce((sum, item) => {
             return sum + (item === 'O' ? i + 1 : 0);
         }, 0);
     }, 0);
+}
+
+function tilt(platform, instruction) {
+    for (let y = 0; y < platform.length; y++) {
+        for (let x = 0; x < platform[0].length; x++) {
+            let targetX = instruction.xAdd > 0 ? platform[0].length - 1 - x : x;
+            let targetY = instruction.yAdd > 0 ? platform.length - 1 - y : y;
+            if (platform[targetY][targetX] !== 'O') continue;
+            let xx = targetX;
+            let yy = targetY;
+            platform[targetY][targetX] = '.';
+            while (true) {
+                xx += instruction.xAdd;
+                yy += instruction.yAdd;
+                if (xx < 0 || yy < 0 || xx == platform[0].length || yy == platform.length || ['O', '#'].includes(platform[yy][xx])) {
+                    break;
+                }
+                targetX = xx;
+                targetY = yy;
+            }
+            platform[targetY][targetX] = 'O';
+        }
+    }
+    instructions.push(instructions.shift());
 }
--- 2023/day14/day14.spec.js
+++ 2023/day14/day14.spec.js
@@ -4,7 +4,8 @@ import run from './day14.js';
 describe('day14', () => {
 
     it('runs', () => {
-        expect(run(readLines('2023/day14/testdata.txt'), 0)).toEqual(109654);
+        // expect(run(readLines('2023/day14/testdata.txt'), 1)).toEqual(109654);
+        expect(run(readLines('2023/day14/testdata.txt'), 2)).toEqual(1000000000);
     });
 
 });
