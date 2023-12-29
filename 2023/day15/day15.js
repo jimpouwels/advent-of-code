b92b26405a53aab4de8e6e0e12e4ec0b2b476822
Jim Pouwels <jim.pouwels@philips.com>
2023-12-29 13:54:20 +0100

    day15

--- 2023/day15/day15.js
+++ 2023/day15/day15.js
@@ -1,24 +1,23 @@
 import { upsert } from "../../common/arrays";
 
 export default function run(input) {
-    let sequence = input.split(',');
+    let sequence = input.split(',').map(step => parseStep(step));
     
     let boxes = Array(256).fill(null);
     boxes.forEach((_, i) => boxes[i] = {lenses: []});
 
-    sequence.map(step => parseStep(step))
-            .forEach(step => {
-                        let box = boxes[hash(step.label)];
-                        switch (step.operation) {
-                            case "=":
-                                upsert(box.lenses, {label: step.label, focal: step.value}, (l) => l.label === step.label);
-                                break;
-                            default:
-                                box.lenses = box.lenses.filter((l) => l.label !== step.label);
-                        }
+    sequence.forEach(step => {
+        let box = boxes[hash(step.label)];
+        switch (step.operation) {
+            case "=":
+                upsert(box.lenses, {label: step.label, focal: step.value}, (l) => l.label === step.label);
+                break;
+            default:
+                box.lenses = box.lenses.filter((l) => l.label !== step.label);
+        }
     });
     return {
-        part1: sequence.reduce((sum, string) => sum + hash(string), 0),
+        part1: sequence.reduce((sum, step) => sum + hash(step.full), 0),
         part2: boxes.reduce((sum, box, i) => 
                              sum + box.lenses.reduce((sum, lens, j) => 
                              sum + ((i + 1) * (j + 1) * lens.focal), 0), 0)
@@ -31,5 +30,5 @@ function hash(value) {
 
 function parseStep(step) {
     const { label, operation, value } = step.match(/(?<label>.*)(?<operation>[=-])(?<value>.*)/).groups;
-    return {label: label, operation: operation, value: parseInt(value)};
+    return {label: label, operation: operation, value: parseInt(value), full: step};
 }
