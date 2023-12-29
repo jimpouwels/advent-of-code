8123d55be2e5db75d4750fd9d7a932f7aa7dc546
Jim Pouwels <jim.pouwels@philips.com>
2023-12-29 20:53:45 +0100

    day16

--- 2023/day16/day16.spec.js
+++ 2023/day16/day16.spec.js
@@ -6,7 +6,7 @@ describe('day16', () => {
     it('runs', () => {
         let result = run(readLines('2023/day16/testdata.txt'));
         expect(result.part1).toEqual(7562);
-        expect(result.part2).toEqual(51);
+        expect(result.part2).toEqual(7793);
     });
 
 });
