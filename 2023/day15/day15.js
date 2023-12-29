ea189ec31265bbe8c4ee6c5c8721b09a14ded803
Jim Pouwels <jim.pouwels@philips.com>
2023-12-29 13:11:50 +0100

    day15

--- 2023/day15/day15.js
+++ 2023/day15/day15.js
@@ -9,8 +9,34 @@ export default function run(input) {
         return sum + hash(string);
     }, 0);
 
+    let boxes = Array(256);
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i] = new Box();
+    }
+
+    sequence.forEach(step => {
+        if (step.includes('=')) {
+            let splitted = step.split('=');
+            let boxIndex = hash(splitted[0]);
+            let box = boxes[boxIndex];
+            box.insert(splitted[0], parseInt(splitted[1]));
+        } else {
+            let label = step.split('-')[0]
+            let boxIndex = hash(label);
+            let box = boxes[boxIndex];
+            box.remove(label);
+        }
+    });
+
+    let part2 = 0;
+    boxes.forEach((box, i) => {
+        box.lenses.forEach((lens, j) => {
+            part2 += ((i + 1) * (j + 1) * lens.focalLength);
+        });
+    });
     return {
-        part1: part1
+        part1: part1,
+        part2: part2
     }
 }
 
@@ -22,4 +48,31 @@ function hash(value) {
         currentValue %= 256;
     });
     return currentValue;
+}
+
+class Box {
+    lenses = [];
+
+    insert(label, focalLength) {
+        let existingLens = this.lenses.filter(l => l.label === label);
+        if (existingLens.length == 0) {
+            this.lenses.push(new Lens(label, focalLength));
+        } else {
+            existingLens[0].focalLength = focalLength;
+        }
+    }
+
+    remove(label) {
+        this.lenses = this.lenses.filter(l => l.label !== label);
+    }
+}
+
+class Lens {
+    label;
+    focalLength;
+
+    constructor(label, focalLength) {
+        this.label = label;
+        this.focalLength = focalLength;
+    }
 }
--- 2023/day15/day15.spec.js
+++ 2023/day15/day15.spec.js
@@ -5,7 +5,8 @@ describe('day15', () => {
 
     it('runs', () => {
         let result = run(readFile('2023/day15/testdata.txt'));
-        expect(result.part1).toEqual(1320);
+        expect(result.part1).toEqual(518107);
+        expect(result.part2).toEqual(303404);
     });
 
 });
