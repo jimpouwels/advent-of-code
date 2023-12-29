9135af775c683c96111dbce88365d09e93a5c01a
Jim Pouwels <jim.pouwels@philips.com>
2023-12-29 13:22:27 +0100

    day15

--- 2023/day15/day15.js
+++ 2023/day15/day15.js
@@ -3,12 +3,6 @@ import Box from "./box";
 export default function run(input) {
     let sequence = input.split(',');
     
-    // PART 1
-    let part1 = sequence.reduce((sum, string) => {
-        return sum + hash(string);
-    }, 0);
-
-    // PART 2
     let boxes = Array(256).fill(null);
     boxes.forEach((_, i) => boxes[i] = new Box());
 
@@ -22,12 +16,11 @@ export default function run(input) {
         }
     });
 
-    let part2 = boxes.reduce((sum, box, i) => 
-                              sum + box.lenses.reduce((sum, lens, j) => 
-                              sum + ((i + 1) * (j + 1) * lens.focalLength), 0), 0);
     return {
-        part1: part1,
-        part2: part2
+        part1: sequence.reduce((sum, string) => sum + hash(string), 0),
+        part2: boxes.reduce((sum, box, i) => 
+                             sum + box.lenses.reduce((sum, lens, j) => 
+                             sum + ((i + 1) * (j + 1) * lens.focalLength), 0), 0)
     }
 }
 
