767936b804b79b7273fc4d7cacb0d2399671abef
Jim Pouwels <jim.pouwels@gmail.com>
2024-11-26 13:08:30 +0100

    refactor

--- 2015/day01/day1.js
+++ 2015/day01/day1.js
@@ -1,8 +1,8 @@
 export default function day1(input) {
-    let basementEntry = Number.MAX_VALUE;
+    let basementEntry = -1;
     let destination = input.split('').reduce((currentFloor, direction, i) => {
         currentFloor += (direction === '(' ? 1 : -1);
-        if (currentFloor < 0 && basementEntry == Number.MAX_VALUE) basementEntry = i + 1;
+        if (currentFloor < 0 && basementEntry == -1) basementEntry = i + 1;
         return currentFloor;
     }, 0);
     return {
