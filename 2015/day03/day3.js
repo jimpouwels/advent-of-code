15493f86ed8a6b5589cd794733d0e52fc33f8690
Jim Pouwels <jim.pouwels@gmail.com>
2024-11-28 15:25:55 +0100

    2015-day3-p2

--- 2015/day03/day3.js
+++ 2015/day03/day3.js
@@ -1,22 +1,33 @@
-export default function day3(input) {
+export default function day3(input, delivererCount) {
     let commands = input.split('');
     let houses = [];
+    let currentDeliverer = 0;
+    let deliverers = Array(delivererCount).fill(null);
+    deliverers.forEach((_, i) => deliverers[i] = new Deliverer());
 
-    let posX = 0, posY = 0;
     houses.push(new House(0, 0));
+    houses[0].presents++;
+
     commands.forEach(c => {
-        if (c === '>') posX++;
-        if (c === '<') posX--;
-        if (c === 'v') posY++;
-        if (c === '^') posY--;
-        let house = houses.filter((h) => h.x == posX && h.y == posY)[0];
+        let deliverer = deliverers[currentDeliverer];
+        if (c === '>') deliverer.posX++;
+        if (c === '<') deliverer.posX--;
+        if (c === 'v') deliverer.posY++;
+        if (c === '^') deliverer.posY--;
+        let house = houses.filter((h) => h.x == deliverer.posX && h.y == deliverer.posY)[0];
         if (house) house.presents++
-        else houses.push(new House(posX, posY));
+        else houses.push(new House(deliverer.posX, deliverer.posY));
+        currentDeliverer++;
+        if (currentDeliverer == delivererCount) {
+            currentDeliverer = 0;
+        }
     });
-    return {
-        part1: houses.filter((h) => h.presents > 0).length,
-        part2: 0
-    }
+    return houses.filter((h) => h.presents > 0).length;
+}
+
+class Deliverer {
+    posX = 0;
+    posY = 0;
 }
 
 class House {
--- 2015/day03/day3.spec.js
+++ 2015/day03/day3.spec.js
@@ -4,9 +4,8 @@ import day3 from './day3.js';
 describe('day3', () => {
 
     it('runs', () => {
-        let result = day3(readFile('2015/day03/testdata.txt'));
-        expect(result.part1).toEqual(2572);
-        expect(result.part2).toEqual(0);
+        expect(day3(readFile('2015/day03/testdata.txt'), 1)).toEqual(2572);
+        expect(day3(readFile('2015/day03/testdata.txt'), 2)).toEqual(0);
     });
 
 });
