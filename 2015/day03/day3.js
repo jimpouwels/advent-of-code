c2a400294536c0e3cb9a6a95501ff0bdd4db59bb
Jim Pouwels <jim.pouwels@gmail.com>
2024-11-28 15:29:35 +0100

    refactor

--- 2015/day03/day3.js
+++ 2015/day03/day3.js
@@ -1,12 +1,9 @@
 export default function day3(input, delivererCount) {
     let commands = input.split('');
-    let houses = [];
+    let houses = Array(1).fill(new House(0, 0));
     let currentDeliverer = 0;
     let deliverers = Array(delivererCount).fill(null);
     deliverers.forEach((_, i) => deliverers[i] = new Deliverer());
-
-    houses.push(new House(0, 0));
-    houses[0].presents++;
 
     commands.forEach(c => {
         let deliverer = deliverers[currentDeliverer];
@@ -14,10 +11,7 @@ export default function day3(input, delivererCount) {
         let house = houses.filter((h) => h.x == deliverer.posX && h.y == deliverer.posY)[0];
         if (house) house.presents++
         else houses.push(new House(deliverer.posX, deliverer.posY));
-        currentDeliverer++;
-        if (currentDeliverer == delivererCount) {
-            currentDeliverer = 0;
-        }
+        currentDeliverer = ++currentDeliverer == delivererCount ? 0 : currentDeliverer;
     });
     return houses.filter((h) => h.presents > 0).length;
 }
