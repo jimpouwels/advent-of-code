2d4a0aed14bd9fd4c6ffa6e8f58ed66d97fb18fc
Jim Pouwels <jim.pouwels@gmail.com>
2024-11-29 06:56:19 +0100

    refactor

--- 2015/day03/day3.js
+++ 2015/day03/day3.js
@@ -1,5 +1,5 @@
 export default function day3(input, delivererCount) {
-    let houses = Array(1).fill(new House(0, 0));
+    let houses = Array(1).fill({ x: 0, y: 0 });
     let deliverers = Array(delivererCount).fill(null);
     deliverers.forEach((_, i) => deliverers[i] = new Deliverer());
 
@@ -7,15 +7,13 @@ export default function day3(input, delivererCount) {
     input.split('').forEach(c => {
         let deliverer = deliverers[currentDeliverer];
         deliverer.handleCommand(c);
-        let house = houses.filter(h => h.isAtPos(deliverer))[0];
-        if (house)
-            house.presents++
-        else
-            houses.push(new House(deliverer.x, deliverer.y));
+        let house = houses.filter(h => h.x == deliverer.x && h.y == deliverer.y)[0];
+        if (!house)
+            houses.push({ x: deliverer.x, y: deliverer.y });
 
         currentDeliverer = ++currentDeliverer == delivererCount ? 0 : currentDeliverer;
     });
-    return houses.filter(h => h.presents > 0).length;
+    return houses.length;
 }
 
 class Deliverer {
@@ -27,20 +25,5 @@ class Deliverer {
         if (command === '<') this.x--;
         if (command === 'v') this.y++;
         if (command === '^') this.y--;
-    }
-}
-
-class House {
-    x = 0;
-    y = 0;
-    presents = 1;
-
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    isAtPos(deliverer) {
-        return this.x == deliverer.x && this.y == deliverer.y;
     }
 }
