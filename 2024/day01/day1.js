d2f82876aecfbfdb387017965d083e77a9126cd2
Jim Pouwels <jim.pouwels@gmail.com>
2024-12-01 14:39:04 +0100

    refactor

--- 2024/day01/day1.js
+++ 2024/day01/day1.js
@@ -3,7 +3,7 @@ export default function run(input) {
     let rightList = input.map(l => l.split('   ')[1]).sort();
 
     return {
-        part1: leftList.reduce((sum, left, i) => sum + (Math.abs(left - rightList[i])), 0),
-        part2: leftList.reduce((sum, left, i) => sum + (left * rightList.filter(r => r == left).length), 0)
+        part1: leftList.reduce((sum, l, i) => sum + (Math.abs(l - rightList[i])), 0),
+        part2: leftList.reduce((sum, l) => sum + (l * rightList.filter(r => r == l).length), 0)
     }
 }
