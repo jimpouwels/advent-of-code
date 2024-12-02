7dc0c081755c4b9d33f622ecf9387e05b4caad22
Jim Pouwels <jim.pouwels@gmail.com>
2024-12-02 20:02:49 +0100

    refactor

--- 2015/day04/day4.js
+++ 2015/day04/day4.js
@@ -1,10 +1,10 @@
 import MD5 from "crypto-js/md5";
 
 export default function day4(input, expectedZeroes) {
-    let zeroes = Array(expectedZeroes).fill().reduce(sum => sum + '0', '');
+    let leadingZeroes = Array(expectedZeroes).fill().reduce(sum => sum + '0', '');
     let i = 0;
     while (i++ >= 0) {
-        if (MD5(`${input}${i}`).toString().startsWith(zeroes)) {
+        if (MD5(`${input}${i}`).toString().startsWith(leadingZeroes)) {
             return i;
         }
     }
