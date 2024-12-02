6ce5c41646fe25b779b6d4f8c1d3f42eafef0896
Jim Pouwels <jim.pouwels@gmail.com>
2024-12-02 20:08:05 +0100

    update

--- 2015/day04/day4.spec.js
+++ 2015/day04/day4.spec.js
@@ -5,7 +5,7 @@ describe('day4', () => {
     it('runs', () => {
         expect(day4('iwrupvqb', 5)).toEqual(346386);
         // Takes too long for the tests to be quick, uncomment to get the result
-        // expect(day4('iwrupvqb', 6)).toEqual(609043);
+        // expect(day4('iwrupvqb', 6)).toEqual(9958218);
     });
 
 });
