0f23ac9ffc04ff3a0b342773c822f62176c08003
Jim Pouwels <jim.pouwels@gmail.com>
2024-12-03 09:30:31 +0100

    2024-day3-part2

--- 2024/day03/day3.js
+++ 2024/day03/day3.js
@@ -1,4 +1,21 @@
-export default function run(input) {
+export function part1(input) {
     return input.matchAll(/mul\((?<d1>(\d+)),(?<d2>(\d+))\)/g).reduce((sum, val) =>
         sum + (parseInt(val.groups.d1) * parseInt(val.groups.d2)), 0);
+}
+
+export function part2(input) {
+    let on = true;
+    return input.matchAll(/mul\((?<d1>(\d+)),(?<d2>(\d+))\)|don't\(\)|do\(\)/g).reduce((sum, val) => {
+        switch (val[0]) {
+            case 'do()':
+                on = true;
+                break;
+            case 'don\'t()':
+                on = false;
+                break;
+            default:
+                sum += on ? (parseInt(val.groups.d1) * parseInt(val.groups.d2)) : 0;
+        }
+        return sum;
+    }, 0);
 }
--- 2024/day03/day3.spec.js
+++ 2024/day03/day3.spec.js
@@ -1,10 +1,13 @@
 import { readFile } from '../../common/readlines.js';
-import run from './day3.js';
+import { part1, part2 } from './day3.js';
 
 describe('day3', () => {
 
     it('part1', () => {
-        expect(run(readFile('2024/day03/testdata.txt'), false)).toEqual(185797128);
+        expect(part1(readFile('2024/day03/testdata.txt'), false)).toEqual(185797128);
     });
 
+    it('part2', () => {
+        expect(part2(readFile('2024/day03/testdata.txt'), false)).toEqual(89798695);
+    });
 });
