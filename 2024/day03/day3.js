fdfd741ec9057bf47f679726f50f93e14dc7e48b
Jim Pouwels <jim.pouwels@gmail.com>
2024-12-03 09:44:51 +0100

    refactor

--- 2024/day03/day3.js
+++ 2024/day03/day3.js
@@ -3,7 +3,6 @@ export default function run(input, enableSwitch) {
     return input.matchAll(/mul\((?<d1>(\d+)),(?<d2>(\d+))\)|don't\(\)|do\(\)/g).reduce((sum, val) => {
         on |= val[0] == 'do()';
         on = val[0] == 'don\'t()' ? false : on;
-        if (!val.groups.d1) return sum;
-        return sum += on || !enableSwitch ? (parseInt(val.groups.d1) * parseInt(val.groups.d2)) : 0;
+        return sum += val.groups.d1 && (on || !enableSwitch) ? (parseInt(val.groups.d1) * parseInt(val.groups.d2)) : 0;
     }, 0);
 }
