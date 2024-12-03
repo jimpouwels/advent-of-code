f944712e0f0e841c0b107ca9a2a045df2fe2b398
Jim Pouwels <jim.pouwels@gmail.com>
2024-12-03 10:08:38 +0100

    refactor test

--- 2024/day03/day3.spec.js
+++ 2024/day03/day3.spec.js
@@ -3,8 +3,11 @@ import run from './day3.js';
 
 describe('day3', () => {
 
-    it('run', () => {
+    it('part1', () => {
         expect(run(readFile('2024/day03/testdata.txt'), false)).toEqual(185797128);
+    });
+
+    it('part2', () => {
         expect(run(readFile('2024/day03/testdata.txt'), true)).toEqual(89798695);
     });
 });
