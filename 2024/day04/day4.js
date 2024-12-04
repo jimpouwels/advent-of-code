a9116e302c5ac6aa2af6344543ba50280f5d2f2c
Jim Pouwels <jim.pouwels@gmail.com>
2024-12-04 10:01:19 +0100

    extract

--- 2024/day04/day4.js
+++ 2024/day04/day4.js
@@ -1,50 +1,10 @@
+import { Grid } from "./model/grid";
+
 export default function run(lines) {
-    let grid = lines.map(l => l.split(''));
+    let grid = new Grid(lines.map(l => l.split('')));
 
     return {
-        part1: grid.reduce((sum, line, y) =>
-            sum + line.reduce((sum, _, x) => sum + allDirections.filter(direction =>
-                findAdjacentChars(grid, "XMAS".split(''), { x: x, y: y }, direction)).length, 0), 0),
-
-        part2: grid.reduce((sum, line, y) => sum + line.reduce((sum, _, x) =>
-            sum + findX(grid, { x: x, y: y }), 0), 0)
+        part1: grid.findWordCount("XMAS"),
+        part2: grid.findX_MASCount()
     }
 }
-
-function findAdjacentChars(grid, remainingChars, currentPosition, direction) {
-    if (grid[currentPosition.y][currentPosition.x] != remainingChars[0]) return false;
-    if (remainingChars.length == 1) return true;
-
-    let nextPosition = { x: currentPosition.x, y: currentPosition.y };
-    nextPosition.x -= direction == Direction.WEST || direction == Direction.NORTH_WEST || direction == Direction.SOUTH_WEST ? 1 : 0;
-    nextPosition.x += direction == Direction.EAST || direction == Direction.NORTH_EAST || direction == Direction.SOUTH_EAST ? 1 : 0;
-    nextPosition.y -= direction == Direction.NORTH || direction == Direction.NORTH_EAST || direction == Direction.NORTH_WEST ? 1 : 0;
-    nextPosition.y += direction == Direction.SOUTH || direction == Direction.SOUTH_EAST || direction == Direction.SOUTH_WEST ? 1 : 0;
-
-    if (nextPosition.x < 0 || nextPosition.x == grid[0].length || nextPosition.y < 0 || nextPosition.y == grid.length) {
-        return false;
-    }
-    return findAdjacentChars(grid, remainingChars.slice(1), nextPosition, direction);
-}
-
-function findX(grid, currentPosition) {
-    if (grid[currentPosition.y][currentPosition.x] != 'A' || currentPosition.x < 1 || currentPosition.x == grid[0].length - 1 || currentPosition.y < 1 || currentPosition.y == grid.length - 1) {
-        return false;
-    }
-    return ((grid[currentPosition.y - 1][currentPosition.x - 1] == 'M' && grid[currentPosition.y + 1][currentPosition.x + 1] == 'S')
-        || (grid[currentPosition.y - 1][currentPosition.x - 1] == 'S' && grid[currentPosition.y + 1][currentPosition.x + 1] == 'M')) &&
-        ((grid[currentPosition.y - 1][currentPosition.x + 1] == 'M' && grid[currentPosition.y + 1][currentPosition.x - 1] == 'S')
-            || (grid[currentPosition.y - 1][currentPosition.x + 1] == 'S' && grid[currentPosition.y + 1][currentPosition.x - 1] == 'M'))
-}
-
-export const Direction = {
-    NORTH: 'North',
-    SOUTH: 'South',
-    WEST: 'West',
-    EAST: 'East',
-    NORTH_WEST: 'NorthWest',
-    NORTH_EAST: 'NorthEast',
-    SOUTH_WEST: 'SoutWest',
-    SOUTH_EAST: 'SouthEast'
-}
-const allDirections = [Direction.NORTH, Direction.SOUTH, Direction.EAST, Direction.WEST, Direction.NORTH_EAST, Direction.NORTH_WEST, Direction.SOUTH_EAST, Direction.SOUTH_WEST];
---
+++ 2024/day04/model/grid.js
@@ -0,0 +1,58 @@
+import { Direction, allDirections } from "./direction";
+
+export class Grid {
+    data;
+
+    constructor(data) {
+        this.data = data;
+    }
+
+    at(x, y) {
+        return this.data[y][x];
+    }
+
+    height() {
+        return this.data.length;
+    }
+
+    width() {
+        return this.data[0].length;
+    }
+
+    findWordCount(word) {
+        return this.data.reduce((sum, line, y) =>
+            sum + line.reduce((sum, _, x) => sum + allDirections.filter(direction =>
+                this.findAdjacentChars(word.split(''), { x: x, y: y }, direction)).length, 0), 0);
+    }
+
+    findX_MASCount() {
+        return this.data.reduce((sum, line, y) => sum + line.reduce((sum, _, x) =>
+            sum + this.findX({ x: x, y: y }), 0), 0)
+    }
+
+    findAdjacentChars(remainingChars, currentPosition, direction) {
+        if (this.at(currentPosition.x, currentPosition.y) != remainingChars[0]) return false;
+        if (remainingChars.length == 1) return true;
+
+        let nextPosition = { x: currentPosition.x, y: currentPosition.y };
+        nextPosition.x -= direction == Direction.WEST || direction == Direction.NORTH_WEST || direction == Direction.SOUTH_WEST ? 1 : 0;
+        nextPosition.x += direction == Direction.EAST || direction == Direction.NORTH_EAST || direction == Direction.SOUTH_EAST ? 1 : 0;
+        nextPosition.y -= direction == Direction.NORTH || direction == Direction.NORTH_EAST || direction == Direction.NORTH_WEST ? 1 : 0;
+        nextPosition.y += direction == Direction.SOUTH || direction == Direction.SOUTH_EAST || direction == Direction.SOUTH_WEST ? 1 : 0;
+
+        if (nextPosition.x < 0 || nextPosition.x == this.width() || nextPosition.y < 0 || nextPosition.y == this.height()) {
+            return false;
+        }
+        return this.findAdjacentChars(remainingChars.slice(1), nextPosition, direction);
+    }
+
+    findX(currentPosition) {
+        if (this.at(currentPosition.x, currentPosition.y) != 'A' || currentPosition.x < 1 || currentPosition.x == this.width() - 1 || currentPosition.y < 1 || currentPosition.y == this.height() - 1) {
+            return false;
+        }
+        return ((this.at(currentPosition.x - 1, currentPosition.y - 1) == 'M' && this.at(currentPosition.x + 1, currentPosition.y + 1) == 'S')
+            || (this.at(currentPosition.x - 1, currentPosition.y - 1) == 'S' && this.at(currentPosition.x + 1, currentPosition.y + 1) == 'M')) &&
+            ((this.at(currentPosition.x + 1, currentPosition.y - 1) == 'M' && this.at(currentPosition.x - 1, currentPosition.y + 1) == 'S')
+                || (this.at(currentPosition.x + 1, currentPosition.y - 1) == 'S' && this.at(currentPosition.x - 1, currentPosition.y + 1) == 'M'))
+    }
+}
