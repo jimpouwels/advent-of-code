7dcfedd66dba926214486ec607dd3990fb8d2286
Jim Pouwels <jim.pouwels@gmail.com>
2024-12-05 10:51:00 +0100

    2024-day5-p2

--- 2024/day05/day5.js
+++ 2024/day05/day5.js
@@ -1,21 +1,31 @@
 import Rule from "./model/rule";
 
-export default function run(input) {
+export default function run(input, fixIncorrect = false) {
     let split = input.split('\n\n');
     let rules = split[0].split('\n').map(l => new Rule(l.split('|')[0], l.split('|')[1]));
     let updates = split[1].split('\n').map(l => l.split(',').map(pn => parseInt(pn)));
 
-    return updates.reduce((sum, update) => {
+    let result = updates.reduce((sum, update) => {
         let allMeet = update.filter((pn, i) => {
             if (i == update.length - 1) return true;
             let meets = true;
             for (let j = i + 1; j < update.length; j++) {
-                if (!rules.every(r => r.meets(pn, update[j]))) {
-                    meets = false;
-                }
+                meets &= rules.every(r => {
+                    let m = r.meets(pn, update[j]);
+                    if (!m && fixIncorrect) {
+                        update[i] = update[j];
+                        update[j] = pn;
+                        pn = update[i];
+                    }
+                    return m;
+                });
             }
             return meets;
         }).length == update.length;
-        return sum += (allMeet ? update[Math.floor(update.length / 2)] : 0);
+        if (fixIncorrect)
+            return sum += (!allMeet ? update[Math.floor(update.length / 2)] : 0);
+        else
+            return sum += (allMeet ? update[Math.floor(update.length / 2)] : 0);
     }, 0);
+    return result;
 }
