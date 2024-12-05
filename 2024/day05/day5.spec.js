8b9fb94d4bc579bce5c012ed6c8b256c1eebf3e8
Jim Pouwels <jim.pouwels@gmail.com>
2024-12-05 10:59:41 +0100

    update

--- 2024/day05/day5.spec.js
+++ 2024/day05/day5.spec.js
@@ -4,10 +4,10 @@ import run from './day5.js';
 describe('day5', () => {
 
     it('part1', () => {
-        expect(run(readFile('2024/day05/testdata.txt'))).toEqual(143);
+        expect(run(readFile('2024/day05/testdata.txt'))).toEqual(5129);
     });
 
     it('part2', () => {
-        expect(run(readFile('2024/day05/testdata.txt'), true)).toEqual(123);
+        expect(run(readFile('2024/day05/testdata.txt'), true)).toEqual(4077);
     });
 });
