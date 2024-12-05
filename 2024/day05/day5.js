f809a7a12dcbfba9cfffbbd8c5da956169dde072
Jim Pouwels <jim.pouwels@gmail.com>
2024-12-05 12:25:58 +0100

    refactor

--- 2024/day05/day5.js
+++ 2024/day05/day5.js
@@ -7,20 +7,17 @@ export default function run(input, fixIncorrect = false) {
     let updates = split[1].split('\n').map(l => l.split(',').map(pn => parseInt(pn)));
 
     let result = updates.reduce((sum, update) => {
-        let allMeet = update.filter((pn1, i) => {
+        let allMeet = update.filter((_, i) => {
             if (i == update.length - 1) return true;
-            let meets = true;
-            for (let j = i + 1; j < update.length; j++) {
-                meets &= rules.every(r => {
-                    let m = r.meets(pn1, update[j]);
+            return update.slice(i + 1).filter((_, j) =>
+                rules.every(r => {
+                    let m = r.meets(update[i], update[i + j + 1]);
                     if (!m && fixIncorrect) {
-                        swap(update, i, j);
-                        pn1 = update[i];
+                        swap(update, i, i + j + 1);
                     }
                     return m;
-                });
-            }
-            return meets;
+                })
+            ).length == update.slice(i + 1).length;
         }).length == update.length;
         return sum += (!allMeet && fixIncorrect) || (allMeet && !fixIncorrect) ? update[Math.floor(update.length / 2)] : 0;
     }, 0);
