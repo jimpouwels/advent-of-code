1410d3edad21e17ac237ea6a36f29c7a4badb8f8
Jim Pouwels <jim.pouwels@gmail.com>
2024-12-06 20:34:33 +0100

    refactor

--- 2024/day06/day6.spec.js
+++ 2024/day06/day6.spec.js
@@ -5,6 +5,6 @@ describe('day6', () => {
 
     it('part1', () => {
         expect(part1(readLines('2024/day06/testdata.txt'))).toEqual(4977);
-        expect(part2(readLines('2024/day06/testdata.txt'))).toEqual(1729); // takes ~15 seconds to run, uncomment to get answer
+        // expect(part2(readLines('2024/day06/testdata.txt'))).toEqual(1729); // takes ~15 seconds to run, uncomment to get answer
     });
 });
