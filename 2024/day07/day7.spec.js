3f2d572954ecea17e02db0bd0e7bd1565a38991a
Jim Pouwels <jim.pouwels@gmail.com>
2024-12-07 21:11:00 +0100

    test

--- 2024/day07/day7.spec.js
+++ 2024/day07/day7.spec.js
@@ -4,7 +4,7 @@ import run from './day7.js';
 describe('day7', () => {
 
     it('run', () => {
-        expect(run(readLines('2024/day07/testdata.txt'))).toEqual(3749);
+        expect(run(readLines('2024/day07/testdata.txt'))).toEqual(3598800864292);
     });
 
 });
