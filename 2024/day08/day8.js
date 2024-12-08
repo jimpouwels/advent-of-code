c1541c1354a122d5153c928706596b891b444588
Jim Pouwels <jim.pouwels@gmail.com>
2024-12-08 13:45:52 +0100

    refactor

--- 2024/day08/day8.js
+++ 2024/day08/day8.js
@@ -3,14 +3,14 @@ import { Grid } from "./model/grid";
 export default function run(input, anyDistance = false) {
     let grid = new Grid(input.map(l => l.split('')));
     return grid.rows().reduce((antinodes, r) => {
-        r.filter(p => p.value !== '.').forEach(position => {
-            grid.find(position.value).filter(p => !p.equals(position)).forEach(otherPosition => {
+        r.filter(p => p.value !== '.').forEach(antenna => {
+            grid.find(antenna.value).filter(otherAntenna => !otherAntenna.equals(antenna)).forEach(otherAntenna => {
                 if (anyDistance) {
-                    antinodes.add(otherPosition);
+                    antinodes.add(otherAntenna);
                 }
-                let deltaX = otherPosition.x - position.x;
-                let deltaY = otherPosition.y - position.y;
-                let nextAntinode = position;
+                let deltaX = otherAntenna.x - antenna.x;
+                let deltaY = otherAntenna.y - antenna.y;
+                let nextAntinode = antenna;
                 while (true) {
                     nextAntinode = grid.at(nextAntinode.x - deltaX, nextAntinode.y - deltaY);
                     if (nextAntinode) antinodes.add(nextAntinode); else break;
