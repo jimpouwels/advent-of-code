d33f9d9e82c604aacf9add902a22aa80450733f5
Jim Pouwels <jim.pouwels@gmail.com>
2024-12-08 09:47:03 +0100

    refactor

--- 2024/day07/day7.js
+++ 2024/day07/day7.js
@@ -3,35 +3,32 @@ export default function run(input, possibleOperators) {
         let split = l.split(':');
         let result = parseInt(split[0]);
         let numbers = split[1].trim().split(' ').map(v => parseInt(v));
-        let operators = operatorList(possibleOperators, numbers.length - 1);
-        if (operators.reduce((sum, o) => {
-            if (numbers.slice(1).reduce((fSum, n, j) => {
-                let char = o.charAt(j);
-                if (char == "+") {
-                    fSum += n;
-                } else if (char == "*") {
-                    fSum *= n;
-                } else {
-                    fSum = parseInt(fSum.toString() + n.toString());
-                }
-                return fSum;
-            }, numbers[0]) == result) {
-                sum += result;
-            }
-            return sum;
-        }, 0) > 0) {
-            sum += result;
+        let combinations = operatorCombinations(possibleOperators, numbers.length - 1);
+        if (combinations.reduce((sum, o) => sum + (numbers.slice(1)
+            .reduce((fSum, n, j) => calculate(fSum, n, o.charAt(j)), numbers[0]) == result) ? result : 0, 0) > 0) {
+            return sum + result;
         }
         return sum;
     }, 0);
 }
 
-function operatorList(possibleOperators, depth, current = '') {
+function calculate(val1, val2, operator) {
+    if (operator == "+") {
+        val1 += val2;
+    } else if (operator == "*") {
+        val1 *= val2;
+    } else {
+        val1 = parseInt(val1.toString() + val2.toString());
+    }
+    return val1;
+}
+
+function operatorCombinations(possibleOperators, depth, current = '') {
     if (current.length === depth) return [current];
     let combinations = [];
 
     possibleOperators.forEach(o => {
-        operatorList(possibleOperators, depth, current + o).forEach(r => {
+        operatorCombinations(possibleOperators, depth, current + o).forEach(r => {
             combinations.push(r);
         });
     });
