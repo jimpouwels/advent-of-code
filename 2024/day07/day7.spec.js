f4fe6d0d8297c348ae07bb5f8c6bffbd35ac517a
Jim Pouwels <jim.pouwels@gmail.com>
2024-12-08 09:56:25 +0100

    comment

--- 2024/day07/day7.spec.js
+++ 2024/day07/day7.spec.js
@@ -5,7 +5,7 @@ describe('day7', () => {
 
     it('run', () => {
         expect(run(readLines('2024/day07/testdata.txt'), ['+', '*'])).toEqual(3598800864292);
-        expect(run(readLines('2024/day07/testdata.txt'), ['+', '*', '|'])).toEqual(340362529351427);
+        // expect(run(readLines('2024/day07/testdata.txt'), ['+', '*', '|'])).toEqual(340362529351427); // takes 12 seconds to run, uncomment if you want to include it
     });
 
 });
