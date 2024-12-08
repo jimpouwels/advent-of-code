fe08a0497f53d6c376ea7414edea3e227209d0d3
Jim Pouwels <jim.pouwels@gmail.com>
2024-12-08 12:30:29 +0100

    refactor

--- 2024/day08/day8.js
+++ 2024/day08/day8.js
@@ -3,22 +3,20 @@ import { Grid } from "./model/grid";
 export default function run(input, anyDistance = false) {
     let grid = new Grid(input.map(l => l.split('')));
     return grid.rows().reduce((antinodes, r) => {
-        r.forEach(position => {
-            if (position.value !== '.') {
-                grid.find(position.value).forEach(otherPosition => {
-                    if (position.equals(otherPosition)) return;
-                    if (anyDistance) {
-                        antinodes.add(otherPosition);
-                    }
-                    let deltaX = otherPosition.x - position.x;
-                    let deltaY = otherPosition.y - position.y;
-                    let nextAntinode = grid.at(position.x - deltaX, position.y - deltaY);
-                    while (nextAntinode) {
-                        antinodes.add(nextAntinode);
-                        nextAntinode = anyDistance ? grid.at(nextAntinode.x - deltaX, nextAntinode.y - deltaY) : null;
-                    }
-                });
-            }
+        r.filter(p => p.value !== '.').forEach(position => {
+            grid.find(position.value).forEach(otherPosition => {
+                if (position.equals(otherPosition)) return;
+                if (anyDistance) {
+                    antinodes.add(otherPosition);
+                }
+                let deltaX = otherPosition.x - position.x;
+                let deltaY = otherPosition.y - position.y;
+                let nextAntinode = grid.at(position.x - deltaX, position.y - deltaY);
+                while (nextAntinode) {
+                    antinodes.add(nextAntinode);
+                    nextAntinode = anyDistance ? grid.at(nextAntinode.x - deltaX, nextAntinode.y - deltaY) : null;
+                }
+            });
         })
         return antinodes;
     }, new Set()).size;
