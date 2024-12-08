83bbcb6e3459e1436f74edf1796e4c42deb12e2d
Jim Pouwels <jim.pouwels@gmail.com>
2024-12-08 09:33:09 +0100

    2024-day7-p2

--- 2024/day07/day7.js
+++ 2024/day07/day7.js
@@ -1,23 +1,30 @@
-export default function run(input) {
+export default function run(input, possibleOperators) {
     let total = 0;
-    let formulas = input.map(l => {
+    input.forEach(l => {
         let split = l.split(':');
         let result = parseInt(split[0]);
         let numbers = split[1].trim().split(' ').map(v => parseInt(v));
-        let operators = operatorList(numbers.length - 1);
+        let operators = operatorList(possibleOperators, numbers.length - 1);
         if (operators.reduce((sum, o) => {
             if (numbers.slice(0, -1).reduce((fSum, n, j) => {
-                if (o.charAt(j) == "+") {
+                let char = o.charAt(j);
+                if (char == "+") {
                     if (fSum == 0) {
                         fSum += (n + numbers[j + 1])
                     } else {
                         fSum += numbers[j + 1];
                     }
-                } else {
+                } else if (char == "*") {
                     if (fSum == 0) {
                         fSum += (n * numbers[j + 1])
                     } else {
                         fSum *= numbers[j + 1];
+                    }
+                } else {
+                    if (fSum == 0) {
+                        fSum += parseInt(n.toString() + numbers[j + 1].toString());
+                    } else {
+                        fSum = parseInt(fSum.toString() + numbers[j + 1].toString());
                     }
                 }
                 return fSum;
@@ -33,12 +40,12 @@ export default function run(input) {
     return total;
 }
 
-function operatorList(depth, current = '') {
+function operatorList(possibleOperators, depth, current = '') {
     if (current.length === depth) return [current];
     let combinations = [];
 
-    ['+', '*'].forEach(o => {
-        operatorList(depth, current + o).forEach(r => {
+    possibleOperators.forEach(o => {
+        operatorList(possibleOperators, depth, current + o).forEach(r => {
             combinations.push(r);
         });
     });
--- 2024/day07/day7.spec.js
+++ 2024/day07/day7.spec.js
@@ -4,7 +4,8 @@ import run from './day7.js';
 describe('day7', () => {
 
     it('run', () => {
-        expect(run(readLines('2024/day07/testdata.txt'))).toEqual(3598800864292);
+        expect(run(readLines('2024/day07/testdata.txt'), ['+', '*'])).toEqual(3598800864292);
+        expect(run(readLines('2024/day07/testdata.txt'), ['+', '*', '|'])).toEqual(340362529351427);
     });
 
 });
