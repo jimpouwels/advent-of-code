3bf47ba37e54f10a9b7ee237c4e2f3ecf1b6ec5e
Jim Pouwels <jim.pouwels@gmail.com>
2024-12-08 12:29:48 +0100

    refactor

--- 2024/day08/day8.js
+++ 2024/day08/day8.js
@@ -2,23 +2,24 @@ import { Grid } from "./model/grid";
 
 export default function run(input, anyDistance = false) {
     let grid = new Grid(input.map(l => l.split('')));
-    let total = new Set();
-    grid.rows().forEach(r => r.forEach(position => {
-        if (position.value !== '.') {
-            grid.find(position.value).forEach(otherPosition => {
-                if (position.equals(otherPosition)) return;
-                if (anyDistance) {
-                    total.add(otherPosition);
-                }
-                let deltaX = otherPosition.x - position.x;
-                let deltaY = otherPosition.y - position.y;
-                let nextAntinode = grid.at(position.x - deltaX, position.y - deltaY);
-                while (nextAntinode) {
-                    total.add(nextAntinode);
-                    nextAntinode = anyDistance ? grid.at(nextAntinode.x - deltaX, nextAntinode.y - deltaY) : null;
-                }
-            });
-        }
-    }));
-    return total.size;
+    return grid.rows().reduce((antinodes, r) => {
+        r.forEach(position => {
+            if (position.value !== '.') {
+                grid.find(position.value).forEach(otherPosition => {
+                    if (position.equals(otherPosition)) return;
+                    if (anyDistance) {
+                        antinodes.add(otherPosition);
+                    }
+                    let deltaX = otherPosition.x - position.x;
+                    let deltaY = otherPosition.y - position.y;
+                    let nextAntinode = grid.at(position.x - deltaX, position.y - deltaY);
+                    while (nextAntinode) {
+                        antinodes.add(nextAntinode);
+                        nextAntinode = anyDistance ? grid.at(nextAntinode.x - deltaX, nextAntinode.y - deltaY) : null;
+                    }
+                });
+            }
+        })
+        return antinodes;
+    }, new Set()).size;
 }
