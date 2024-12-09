d2a0b88cada883b8e4b467d899edb6405b8b4732
Jim Pouwels <jim.pouwels@gmail.com>
2024-12-09 10:18:23 +0100

    2024-day9-p2

--- 2024/day09/day9.js
+++ 2024/day09/day9.js
@@ -1,24 +1,81 @@
+import { swap } from "../../common/arrays";
+
 export default function run(input) {
     let id = 0;
-    let places = input.split('').flatMap((p, i) => {
+    let originalMemory = input.split('').flatMap((p, i) => {
         p = parseInt(p);
         if (i % 2 == 0) {
             return Array(p).fill(id++);
         } else {
-            return Array(p).fill(-1);
+            return Array(p).fill('.');
         }
     });
-    places.forEach((element, i) => {
-        if (element == -1)
+    let part1Memory = defragmentPart1(originalMemory);
+    let part2Memory = defragmentPart2(originalMemory);
+
+    return {
+        part1: checksum(part1Memory),
+        part2: checksum(part2Memory)
+    }
+}
+
+function defragmentPart1(memory) {
+    let memCopy = [...memory];
+    memCopy.forEach((element, i) => {
+        if (element == '.')
             while (true) {
-                let end = places.pop();
-                if (end != -1) {
-                    places[i] = end;
+                let end = memCopy.pop();
+                if (end != '.') {
+                    memCopy[i] = end;
                     break;
                 }
             }
     });
-    return places.reduce((sum, val, i) => {
-        return sum + (i * val);
-    }, 0);
+    return memCopy;
+}
+
+function defragmentPart2(memory) {
+    let memCopy = [...memory];
+    let backPos = memCopy.length;
+    let reverse = [...memCopy].reverse();
+    while (reverse.length > 0) {
+        let value = reverse[0];
+        if (value == '.') {
+            reverse.shift();
+            backPos--;
+            continue;
+        }
+        let requiredSpace = 0;
+        while (reverse[0] == value) {
+            requiredSpace++;
+            reverse.shift();
+            backPos--;
+        }
+
+        for (let i = 0; i < backPos; i++) {
+            let element = memCopy[i];
+            if (element != '.') continue;
+            let availableSpace = 0;
+
+            let writeIndex = i;
+            while (memCopy[writeIndex++] == '.') {
+                availableSpace++;
+            }
+            if (availableSpace < requiredSpace) {
+                i += availableSpace;
+                continue;
+            }
+            let j = i;
+            for (let x = 0; x < requiredSpace; x++) {
+                swap(memCopy, j++, backPos + x);
+                i++;
+            }
+            break;
+        };
+    };
+    return memCopy;
+}
+
+function checksum(memory) {
+    return memory.reduce((sum, val, i) => sum + (val != '.' ? (i * val) : 0), 0);
 }
--- 2024/day09/day9.spec.js
+++ 2024/day09/day9.spec.js
@@ -4,7 +4,9 @@ import run from './day9.js';
 describe('day9', () => {
 
     it('run', () => {
-        expect(run(readFile('2024/day09/testdata.txt'))).toEqual(1928);
+        let result = run(readFile('2024/day09/testdata.txt'));
+        expect(result.part1).toEqual(1928);
+        expect(result.part2).toEqual(2858);
     });
 
 });
