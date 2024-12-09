8cc3303256a974212fb25e531dad9858137afe7e
Jim Pouwels <jim.pouwels@gmail.com>
2024-12-09 14:43:33 +0100

    commit

--- 2024/day09/day9.spec.js
+++ 2024/day09/day9.spec.js
@@ -5,7 +5,7 @@ describe('day9', () => {
 
     it('run', () => {
         let result = run(readFile('2024/day09/testdata.txt'));
-        // expect(result.part1).toEqual(6461289671426);
+        expect(result.part1).toEqual(6461289671426);
         expect(result.part2).toEqual(6488291456470);
     });
 
