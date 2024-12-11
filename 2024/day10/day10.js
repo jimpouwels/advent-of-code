0774f25ee645b2d3844aacb4150443f3e9cc3b22
Jim Pouwels <jim.pouwels@gmail.com>
2024-12-11 11:26:25 +0100

    refactor

--- 2024/day10/day10.js
+++ 2024/day10/day10.js
@@ -2,17 +2,17 @@ import Grid from "./model/grid";
 
 export default function run(input) {
     let grid = new Grid(input.map(l => l.split('')));
-    let results = grid.find(0).map(val => findPathsFrom(grid, val));
+    let results = grid.find(0).map(val => findPathsFrom(grid, val, 9));
     return {
         part1: results.reduce((sum, val) => sum + new Set([...val]).size, 0),
         part2: results.reduce((sum, val) => sum + val.length, 0)
     }
 }
 
-function findPathsFrom(grid, position) {
+function findPathsFrom(grid, position, end) {
     if (!position) return [];
-    if (position.value == 9) return [position];
+    if (position.value == end) return [position];
     return [grid.left(position), grid.right(position), grid.above(position), grid.below(position)]
         .filter(p => p && !isNaN(p.value) && p.value - position.value == 1)
-        .flatMap(p => findPathsFrom(grid, p));
+        .flatMap(p => findPathsFrom(grid, p, end));
 }
