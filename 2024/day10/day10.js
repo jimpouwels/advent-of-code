b213d44a06edd6cea3e9377e7b9853f60d6cc4f5
Jim Pouwels <jim.pouwels@gmail.com>
2024-12-11 11:15:33 +0100

    refactor

--- 2024/day10/day10.js
+++ 2024/day10/day10.js
@@ -2,36 +2,24 @@ import { Grid } from "./model/grid";
 
 export default function run(input) {
     let grid = new Grid(input.map(l => l.split('')));
+
+    let results = grid.find(0).map(val => findPathsFrom(grid, val));
     return {
-        part1: grid.find(0).reduce((sum, val) => sum + (findPathsFromP1(grid, val).size), 0),
-        part2: grid.find(0).reduce((sum, val) => sum + (findPathsFromP2(grid, val).length), 0)
+        part1: results.reduce((sum, val) => sum + new Set([...val]).size, 0),
+        part2: results.reduce((sum, val) => sum + val.length, 0)
     }
 }
 
-function findPathsFromP1(grid, position,) {
-    let positions = new Set();
-    if (!position) return positions;
-    if (position.value == 9) {
-        positions.add(position);
-        return positions;
-    }
-    positions = new Set([...positions, ...(findPathsFromP1(grid, grid.left(position, (v) => !isNaN(v.value) && v.value - position.value == 1)))]);
-    positions = new Set([...positions, ...(findPathsFromP1(grid, grid.right(position, (v) => !isNaN(v.value) && v.value - position.value == 1)))]);
-    positions = new Set([...positions, ...(findPathsFromP1(grid, grid.above(position, (v) => !isNaN(v.value) && v.value - position.value == 1)))]);
-    positions = new Set([...positions, ...(findPathsFromP1(grid, grid.below(position, (v) => !isNaN(v.value) && v.value - position.value == 1)))]);
-    return positions;
-}
-
-function findPathsFromP2(grid, position,) {
+function findPathsFrom(grid, position,) {
     let positions = [];
     if (!position) return positions;
     if (position.value == 9) {
         positions.push(position);
         return positions;
     }
-    positions = [...positions, ...(findPathsFromP2(grid, grid.left(position, (v) => !isNaN(v.value) && v.value - position.value == 1)))];
-    positions = [...positions, ...(findPathsFromP2(grid, grid.right(position, (v) => !isNaN(v.value) && v.value - position.value == 1)))];
-    positions = [...positions, ...(findPathsFromP2(grid, grid.above(position, (v) => !isNaN(v.value) && v.value - position.value == 1)))];
-    positions = [...positions, ...(findPathsFromP2(grid, grid.below(position, (v) => !isNaN(v.value) && v.value - position.value == 1)))];
+    positions = [...positions, ...(findPathsFrom(grid, grid.left(position, (v) => !isNaN(v.value) && v.value - position.value == 1)))];
+    positions = [...positions, ...(findPathsFrom(grid, grid.right(position, (v) => !isNaN(v.value) && v.value - position.value == 1)))];
+    positions = [...positions, ...(findPathsFrom(grid, grid.above(position, (v) => !isNaN(v.value) && v.value - position.value == 1)))];
+    positions = [...positions, ...(findPathsFrom(grid, grid.below(position, (v) => !isNaN(v.value) && v.value - position.value == 1)))];
     return positions;
 }
