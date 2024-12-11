fd612d017ff76c8a29cc18b85e77455caff3be82
Jim Pouwels <jim.pouwels@gmail.com>
2024-12-11 11:10:35 +0100

    refactor

--- 2024/day10/day10.js
+++ 2024/day10/day10.js
@@ -2,19 +2,36 @@ import { Grid } from "./model/grid";
 
 export default function run(input) {
     let grid = new Grid(input.map(l => l.split('')));
-    return grid.find(0).reduce((sum, val) => sum + (findPathsFrom(grid, val).size), 0);
+    return {
+        part1: grid.find(0).reduce((sum, val) => sum + (findPathsFromP1(grid, val).size), 0),
+        part2: grid.find(0).reduce((sum, val) => sum + (findPathsFromP2(grid, val).length), 0)
+    }
 }
 
-function findPathsFrom(grid, position) {
+function findPathsFromP1(grid, position,) {
     let positions = new Set();
     if (!position) return positions;
     if (position.value == 9) {
         positions.add(position);
         return positions;
     }
-    positions = new Set([...positions, ...(findPathsFrom(grid, grid.left(position, (v) => !isNaN(v.value) && v.value - position.value == 1)))]);
-    positions = new Set([...positions, ...(findPathsFrom(grid, grid.right(position, (v) => !isNaN(v.value) && v.value - position.value == 1)))]);
-    positions = new Set([...positions, ...(findPathsFrom(grid, grid.above(position, (v) => !isNaN(v.value) && v.value - position.value == 1)))]);
-    positions = new Set([...positions, ...(findPathsFrom(grid, grid.below(position, (v) => !isNaN(v.value) && v.value - position.value == 1)))]);
+    positions = new Set([...positions, ...(findPathsFromP1(grid, grid.left(position, (v) => !isNaN(v.value) && v.value - position.value == 1)))]);
+    positions = new Set([...positions, ...(findPathsFromP1(grid, grid.right(position, (v) => !isNaN(v.value) && v.value - position.value == 1)))]);
+    positions = new Set([...positions, ...(findPathsFromP1(grid, grid.above(position, (v) => !isNaN(v.value) && v.value - position.value == 1)))]);
+    positions = new Set([...positions, ...(findPathsFromP1(grid, grid.below(position, (v) => !isNaN(v.value) && v.value - position.value == 1)))]);
+    return positions;
+}
+
+function findPathsFromP2(grid, position,) {
+    let positions = [];
+    if (!position) return positions;
+    if (position.value == 9) {
+        positions.push(position);
+        return positions;
+    }
+    positions = [...positions, ...(findPathsFromP2(grid, grid.left(position, (v) => !isNaN(v.value) && v.value - position.value == 1)))];
+    positions = [...positions, ...(findPathsFromP2(grid, grid.right(position, (v) => !isNaN(v.value) && v.value - position.value == 1)))];
+    positions = [...positions, ...(findPathsFromP2(grid, grid.above(position, (v) => !isNaN(v.value) && v.value - position.value == 1)))];
+    positions = [...positions, ...(findPathsFromP2(grid, grid.below(position, (v) => !isNaN(v.value) && v.value - position.value == 1)))];
     return positions;
 }
--- 2024/day10/day10.spec.js
+++ 2024/day10/day10.spec.js
@@ -4,7 +4,9 @@ import run from './day10.js';
 describe('day10', () => {
 
     it('run', () => {
-        expect(run(readLines('2024/day10/testdata.txt'))).toEqual(512);
+        let result = run(readLines('2024/day10/testdata.txt'));
+        expect(result.part1).toEqual(512);
+        expect(result.part2).toEqual(1045);
     });
 
 });
